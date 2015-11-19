fedb1f51b45e3101a15d6f39c2254cafa1fc74a9
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2015-11-19 12:34:22 -0500

    up timeout interval

--- webdrivertest/wdio.conf.js
+++ webdrivertest/wdio.conf.js
@@ -60,7 +60,7 @@
         baseUrl: 'http://localhost:8000/webdrivertest/test',
         framework: 'jasmine',
         jasmineNodeOpts: {
-            defaultTimeoutInterval: 85000,
+            defaultTimeoutInterval: 16000,
             expectationResultHandler: function () {
             }
         },
