2d3711f0525cf6b7f20df3e49871c004bc761baa
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2015-11-19 13:18:13 -0500

    revert context menu chaining

--- webdrivertest/test/contextmenu/contextmenu.visual.js
+++ webdrivertest/test/contextmenu/contextmenu.visual.js
@@ -9,46 +9,73 @@ describe('contextmenu', function () {
 
     describe('context menu', function () {
         it('should take contextmenu closed screenshot', function (done) {
+            var screenshotName = 'contextmenu_closed',
+                pageName = screenshotName + '_full';
             browser
                 .url('/contextmenu/fixtures/test.full.html')
-                .webdrivercss('contextmenu_closed_full', [
+                .webdrivercss(pageName, [
                     {
-                        name: 'contextmenu_closed',
+                        name: screenshotName,
                         elem: '#screenshot-contextmenu'
                     }
                 ], function (err, res) {
                     expect(err).toBe(undefined);
-                    expect(res.contextmenu_closed[0].isWithinMisMatchTolerance).toBe(true);
-                })
+                    expect(res[screenshotName][0].isWithinMisMatchTolerance).toBe(true);
+                }).call(done);
+        });
+
+        it('should take contextmenu open screenshot', function (done) {
+            var screenshotName = 'contextmenu_open',
+                pageName = screenshotName + '_full';
+            browser
+                .url('/contextmenu/fixtures/test.full.html')
                 .click('#screenshot-contextmenu button.bb-context-menu-btn')
-                .webdrivercss('contextmenu_open_full', [
+                .webdrivercss(pageName, [
                     {
-                        name: 'contextmenu_open',
+                        name: screenshotName,
                         elem: '#screenshot-contextmenu'
                     }
                 ], function (err, res) {
                     expect(err).toBe(undefined);
-                    expect(res.contextmenu_open[0].isWithinMisMatchTolerance).toBe(true);
-                })
+                    expect(res[screenshotName][0].isWithinMisMatchTolerance).toBe(true);
+                }).call(done);
+        });
+
+    });
+
+    describe('submenu', function () {
+        it('should take the submenu collapsed screenshot', function (done) {
+            var screenshotName = 'submenumenu_collapsed',
+                pageName = screenshotName + '_full';
+            browser
+                .url('/contextmenu/fixtures/test.full.html')
                 .click('#screenshot-submenu button.bb-context-menu-btn')
-                .webdrivercss('submenumenu_collapsed_full', [
+                .webdrivercss(pageName, [
                     {
-                        name: 'submenumenu_collapsed',
+                        name: screenshotName,
                         elem: '#screenshot-submenu'
                     }
                 ], function (err, res) {
                     expect(err).toBe(undefined);
-                    expect(res.submenu_collapsed[0].isWithinMisMatchTolerance).toBe(true);
-                })
+                    expect(res[screenshotName][0].isWithinMisMatchTolerance).toBe(true);
+                }).call(done);
+        });
+
+        it('should take the submenu expanded screenshot', function (done) {
+            var screenshotName = 'submenumenu_expanded',
+                pageName = screenshotName + '_full';
+            browser
+                .url('/contextmenu/fixtures/test.full.html')
+                .click('#screenshot-submenu button.bb-context-menu-btn')
                 .click('#screenshot-submenu .bb-submenu .panel-title .accordion-toggle > span > div')
-                .webdrivercss('submenumenu_expanded_full', [
+                .webdrivercss(pageName, [
                     {
-                        name: 'submenumenu_expanded',
+                        name: screenshotName,
                         elem: '#screenshot-submenu'
                     }
                 ], function (err, res) {
                     expect(err).toBe(undefined);
-                    expect(res.submenumenu_expanded[0].isWithinMisMatchTolerance).toBe(true);
+                    expect(res[screenshotName][0].isWithinMisMatchTolerance).toBe(true);
                 }).call(done);
         });
 
