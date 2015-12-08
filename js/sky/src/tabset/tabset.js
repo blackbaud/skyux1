834a549b674a4f029173fac92f0d15daca536787
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2015-12-08 10:24:42 -0500

    fix merge conflicts

--- js/sky/src/tabset/tabset.js
+++ js/sky/src/tabset/tabset.js
@@ -268,8 +268,10 @@ If you wish to add a close icon to a tab, just add the `bb-tab-close` class to t
 
     angular.module('sky.tabset', ['ui.bootstrap.tabs', 'sky.mediabreakpoints'])
         .directive('uibTabset', tabset)
+        .directive('tabset', tabset)
         .directive('bbTabsetCollapsible', bbTabsetCollapsible)
         .directive('bbTabCollapseHeader', bbTabCollapseHeader)
+        .directive('tab', tab)
         .directive('uibTab', tab);
 
 }(jQuery));
