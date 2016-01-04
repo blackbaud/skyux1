5a41449b4ac4353ccb53e5d43d932d9229a340a0
Travis <travis@travis-ci.org>
2016-01-04 18:25:14 +0000

    Travis build 288 pushed to skyux [ci skip]

--- dist/js/sky-bundle.js
+++ dist/js/sky-bundle.js
@@ -89695,10 +89695,17 @@ If it is necessary to apply action bar stylying to more complicated scenarios (e
 /*global angular */
 
 /** @module Autofocus
+@deprecated
 @icon camera
 @summary  The autofocus component specifies the input item on a form that should get focus when the form loads.
- @description The `bb-autofocus` directive specifies the item on a form that should get focus when the form renders. You can use this directive when items such as Angular dynamically loaded templates do not play nicely with the HTML autofocus property.
+ @description ### *Deprecated* ###
+
+This directive is no longer being maintained. UI-Bootstrap now allows the use of HTML5 autofocus on modals to focus an input upon opening the modal.
+
+<s>
+The `bb-autofocus` directive specifies the item on a form that should get focus when the form renders. You can use this directive when items such as Angular dynamically loaded templates do not play nicely with the HTML autofocus property.
 The **Open Modal** button below demonstrates a modal form where the `bb-autofocus` directive places the focus on an input control on the form.
+</s>
  */
 
 (function () {
