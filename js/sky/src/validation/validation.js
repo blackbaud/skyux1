f818ca7340b2e8a2862ab080114086c38f9fcb7a
blackbaud-johnly <John.Lyons@blackbaud.com>
2016-01-15 14:11:41 -0500

    More style/grammar/wording/formatting edits.

--- js/sky/src/validation/validation.js
+++ js/sky/src/validation/validation.js
@@ -2,13 +2,13 @@
 
 /** @module Email Validation
 @icon check
-@summary The email validation directive allows you to validate email strings in input fields.
- @description The email validation provides the ability to validate email strings in input fields.
+@summary The email validation directive validates email address strings in input fields.
+ @description The email validation directive validates email address strings in input fields.
 
 ### Email Validation Settings ###
 
-- `ng-model` An object to bind the email value to on the input.
-- `type=email` indicates that email validation can be used.
+- `ng-model` &mdash; Specifies an object to bind the email value to on the input.
+- `type=email` &mdash; Indicates that email validation can be used.
  */
 
 (function () {
