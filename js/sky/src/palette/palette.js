58089106c46c58d090805d5fe1b20e9e4bbbb4f4
Bobby Earl <bobby.earl@blackbaud.com>
2016-01-27 11:45:16 -0500

    Added docs

--- js/sky/src/palette/palette.js
+++ js/sky/src/palette/palette.js
@@ -11,7 +11,7 @@
   - `getColorSequence(requestedLength, paletteType)` Returns an array of colors for the requested length.  When using with `ng-repeat`, be sure to use the `track by` syntax since we return duplicates.
     - 'requestedLength' A required integer for the size of the array of colors you want returned.
     - 'paletteType' An optional string representing either `mono` or the default `multi`.
-
+ - Changed
 */
 
 (function () {
