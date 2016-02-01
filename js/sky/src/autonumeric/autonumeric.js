46f9b6de225441a991bee2ac1b5839ceb94474df
blackbaud-johnly <John.Lyons@blackbaud.com>
2016-02-01 12:49:01 -0500

    Style changes and such

--- js/sky/src/autonumeric/autonumeric.js
+++ js/sky/src/autonumeric/autonumeric.js
@@ -2,28 +2,24 @@
 /*global angular, jQuery */
 /** @module Autonumeric
 @icon calculator
-@summary The autonumeric component wraps the autoNumeric jQuery plugin to format any type of number, including currency.
+@summary The autonumeric component wraps up the autoNumeric jQuery plugin to format any type of number.
  @description The `bb-autonumeric` directive wraps up the autoNumeric jQuery plugin to format any type of number, including currency. You must use this directive in conjunction with the `ngModel` directive where the property bound to `ngModel` is the raw numeric value on your model.
 
  ### Dependencies ###
-
- - **[autoNumeric](http://www.decorplanit.com/plugin/) (1.9.27 or higher)** Used to format money values
+ - **[autoNumeric](http://www.decorplanit.com/plugin/) (1.9.27 or higher)** Formats money values.
 
 ---
 
 ### Autonumeric Settings ###
+ - `bb-autonumeric` &mdash; *(Optional.)* Can be assigned the name of a property from the `bbAutonumericConfig` object. It defaults to `number`.
+ - `bb-autonumeric-settings` &mdash; Can be assigned a value that represents a settings object that can be passed to autoNumeric. These options override any default options specified in the `bb-autonumeric` attribute. For more information, see the [complete list of options](http://www.decorplanit.com/plugin/).
 
- - `bb-autonumeric` &mdash; This  can optionally be assigned the name of a property from the `bbAutonumericConfig` object.  If none is specified, it defaults to `number`.
- - `bb-autonumeric-settings` &mdash; This can be assigned a value that represents a settings object that can be passed to autoNumeric. These options override any default options specified in the `bb-autonumeric` attribute. A complete list of options is available [here](http://www.decorplanit.com/plugin/).
+### Autonumeric Filter Settings ###
+In addition to the directive, the autonumeric component includes a filter that can format numbers. The filter can also optionally abbreviate numbers according to SKY UX patterns. For example, it can display 10,000 as 10k, 1,000,000 as 1m, and 1,000,000,000 as 1b. The filter takes three arguments:
 
-### Autonumeric Filter ###
-
-In addition to the directive, there is also a filter that can format numbers. The filter can also optionally abbreviate numbers according to SKY UX patterns. For example,
-it can display 10,000 as 10k, 1,000,000 as 1m, and 1,000,000,000 as 1b. The filter takes three arguments:
-
- - `input` &mdash; The value to format.
- - `configType` &mdash; The name of the configuration (`number` or `money`) to apply to the value.
- - `abbreviate` &mdash; A Boolean value that indicates whether to abbreviate large numbers.
+ - `input` &mdash; Specifies the value to format.
+ - `configType` &mdash; Specifies the name of the configuration (`number` or `money`) to apply to the value.
+ - `abbreviate` &mdash; Provides a Boolean value to indicate whether to abbreviate large numbers.
  */
 (function ($) {
     'use strict';
--- js/sky/src/check/check.js
+++ js/sky/src/check/check.js
@@ -6,6 +6,8 @@
 @icon check-square
 @summary The check applies a commonly styled selector to a checkbox or radio button.
  @description The check directive allows you to change an input element of type checkbox or radio button into a commonly-styled selector. The value that is selected is driven through the `ng-model` attribute specified on the input element. For radio button input types, the value to set on the `ng-model` can be specified by the value attribute.
+ 
+ The `bb-check` directive applies SKY UX styles ot checkboxes and radio buttons.
 
 ---
 
