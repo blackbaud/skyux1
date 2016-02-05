e5174e41105b95b292baf1b1f34d55b5264ecdab
blackbaud-johnly <John.Lyons@blackbaud.com>
2016-02-05 11:42:03 -0500

    Final round of edits to autonumeric component docs after getting feedback from Bobby and Patrick.

--- js/sky/src/autonumeric/autonumeric.js
+++ js/sky/src/autonumeric/autonumeric.js
@@ -11,11 +11,11 @@
 ---
 
 ### Autonumeric Settings ###
- - `bb-autonumeric` &mdash; *(Optional.)* Can be assigned the name of a property from the `bbAutonumericConfig` object. It defaults to `number`.
- - `bb-autonumeric-settings` &mdash; Can be assigned a value that represents a settings object that can be passed to autoNumeric. These options override any default options specified in the `bb-autonumeric` attribute. For more information, see the [complete list of options](http://www.decorplanit.com/plugin/).
+ - `bb-autonumeric` &mdash; *(Optional.)* Assigns the name of a property from the `bbAutonumericConfig` object. *(Default: `number`)*
+ - `bb-autonumeric-settings` &mdash; Specifies a value that represents a settings object to pass to the autoNumeric jQuery plugin. These options override any default options specified in the `bb-autonumeric` attribute. For more information, see the [complete list of options](http://www.decorplanit.com/plugin/).
 
 ### Autonumeric Filter Settings ###
-In addition to the directive, the autonumeric component includes a filter that can format numbers. The filter can also optionally abbreviate numbers according to SKY UX patterns. For example, it can display 10,000 as 10k, 1,000,000 as 1m, and 1,000,000,000 as 1b. The filter takes three arguments:
+In addition to the directive, the autonumeric component includes a filter that can format numbers. The filter can optionally abbreviate numbers according to SKY UX patterns. For example, it can display 10,000 as 10k, 1,000,000 as 1m, and 1,000,000,000 as 1b. The filter takes three arguments:
 
  - `input` &mdash; Specifies the value to format.
  - `configType` &mdash; Specifies the name of the configuration (`number` or `money`) to apply to the value.
