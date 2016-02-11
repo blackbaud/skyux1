fde8c839dea37e312119d372f4d08bfef72e50e5
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-02-11 10:54:11 -0500

    Update tests and documentation for error template #49

--- js/sky/src/error/docs/demo.md
+++ js/sky/src/error/docs/demo.md
@@ -6,22 +6,9 @@ summary: The error component allows a component to show indication of a degraded
 
 The error directive shows indication of a degraded state. The parent `bb-error` directive can contain multiple directives, and each one is optional.
 
-### Image
-You can display an image above the error.
-
-You use the `bb-error-image` directive to display the image.
-
-### Title
-You can display a title for the error that indicates that an error occurred.
-
-You use the `bb-error-title` directive to display the title.
-
-### Description
-You can display a description of the error that gives the user additional details.
-
-You use the `bb-error-description` directive to display the description.
-
-### Action
-You can display an action associated with the error. This might include a button that reloads the page or attempts to refresh data.
-
-You use the `bb-error-action` directive to display the action.
+### Error Settings
+  - `bb-error` &mdash; Wraps the content in a Sky UX-themed error container.
+    - `bb-error-image` &mdash; Place an image in this directive to display it above the error.
+    - `bb-error-title` &mdash; Place content for the title for the error in this directive.
+    - `bb-error-description` &mdash; Place content for the description of an error in this directive to give the user additional details.
+    - `bb-error-action` &mdash; Place an action associated with the error in this directive. This might include a button that reloads the page or attempts to refresh data.
--- js/sky/src/error/test/error.spec.js
+++ js/sky/src/error/test/error.spec.js
@@ -36,10 +36,12 @@ describe('Error', function () {
     it('should be able to show a header, description, image, and action button', function () {
         var actionClicked = false,
             actionEl,
+            imageEl,
+            descriptionEl,
+            headerEl,
             el = angular.element('<bb-error>' +
             '<bb-error-image>' +
-              '<img ng-src="imgUrl">' +
-              '</img>' +
+              '<img ng-src="{{imgUrl}}"/>' +
             '</bb-error-image>' +
             '<bb-error-title>' +
               '{{errorHeader}}' +
@@ -61,21 +63,69 @@ describe('Error', function () {
         $scope.actionName = 'My Action';
 
         $compile(el)($scope);
+        el.appendTo(document.body);
 
         $scope.$digest();
 
-        expect(getErrorImage(el)).toHaveAttr('src', imgUrl);
-        expect(getErrorHeader(el)).toHaveText('My Header');
-        expect(getErrorDescription(el)).toHaveText('My Description');
+        imageEl = getErrorImage(el);
+        expect(imageEl).toBeVisible();
+        expect(imageEl).toHaveAttr('src', imgUrl);
+
+        headerEl = getErrorHeader(el);
+        expect(headerEl).toHaveText('My Header');
+        expect(headerEl).toBeVisible();
+
+        descriptionEl = getErrorDescription(el);
+        expect(descriptionEl).toHaveText('My Description');
+        expect(descriptionEl).toBeVisible();
+
         actionEl = getErrorAction(el);
         expect(actionEl).toHaveText('My Action');
-        expect(actionEl).toHaveClass('bb-btn-secondary');
+        expect(actionEl).toHaveClass('btn-primary');
+        expect(actionEl).toBeVisible();
         actionEl.click();
         expect(actionClicked).toBe(true);
-
+        $scope.$destroy();
+        el.remove();
     });
 
     it('should not show components that are not specified', function () {
+        var actionEl,
+            imageEl,
+            descriptionEl,
+            headerEl,
+            el = angular.element('<bb-error>' +
+            '<bb-error-title>' +
+              '{{errorHeader}}' +
+            '</bb-error-title>' +
+            '<bb-error-description>' +
+              '{{errorDescription}}' +
+            '</bb-error-description>' +
+          '</bb-error>');
+
+        $scope.errorHeader = 'My Header';
+        $scope.errorDescription = 'My Description';
+
+        el.appendTo(document.body);
+
+        $compile(el)($scope);
+
+        $scope.$digest();
+        imageEl = getErrorImage(el);
+        expect(imageEl).not.toBeVisible();
+
+        headerEl = getErrorHeader(el);
+        expect(headerEl).toHaveText('My Header');
+        expect(headerEl).toBeVisible();
+
+        descriptionEl = getErrorDescription(el);
+        expect(descriptionEl).toHaveText('My Description');
+        expect(descriptionEl).toBeVisible();
+
+        actionEl = getErrorAction(el);
+        expect(actionEl).not.toBeVisible();
+
+        el.remove();
 
     });
 
