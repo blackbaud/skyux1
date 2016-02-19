9e0e2c4c41127667d296398a70100efdb495b90b
Travis <travis@travis-ci.org>
2016-02-19 21:13:06 +0000

    Travis build 520 pushed to skyux [ci skip]

--- dist/js/sky.js
+++ dist/js/sky.js
@@ -11,7 +11,7 @@
 (function () {
     'use strict';
 
-    angular.module('sky.avatar', ['sky.avatar.directive']);
+    angular.module('sky.avatar', ['sky.avatar.config', 'sky.avatar.directive']);
 }());
 
 /*global angular */
@@ -19,7 +19,7 @@
 (function () {
     'use strict';
 
-    angular.module('sky.error', ['sky.error.directive']);
+    angular.module('sky.error', ['sky.error.directive', 'sky.errormodal.service']);
 }());
 
 /*global angular */
@@ -404,12 +404,25 @@
         }]);
 }(jQuery));
 
+/*global angular */
+
+(function () {
+    'use strict';
+
+    var bbAvatarConfig = {
+        maxFileSize: 500000
+    };
+
+    angular.module('sky.avatar.config', [])
+        .constant('bbAvatarConfig', bbAvatarConfig);
+}());
+
 /*global angular, jQuery */
 
 (function ($) {
     'use strict';
 
-    function bbAvatar($window, bbPalette) {
+    function bbAvatar($filter, $templateCache, $window, bbAvatarConfig, bbErrorModal, bbFormat, bbPalette, bbResources) {
         function link(scope, el, attrs, vm) {
             var blobUrl,
                 templateLoaded;
@@ -527,14 +540,39 @@
                 }
             }
 
+            function handleInvalidFileDrop(rejectedFile) {
+                var errorDescription,
+                    errorTitle,
+                    maxFileSizeFormatted;
+
+                if (rejectedFile.type.toUpperCase().indexOf('IMAGE/') !== 0) {
+                    errorDescription = bbResources.avatar_error_not_image_description;
+                    errorTitle = bbResources.avatar_error_not_image_title;
+                } else {
+                    maxFileSizeFormatted = $filter('bbFileSize')(bbAvatarConfig.maxFileSize);
+
+                    errorDescription = bbFormat.formatText(bbResources.avatar_error_too_large_description, maxFileSizeFormatted);
+                    errorTitle = bbResources.avatar_error_too_large_title;
+                }
+
+                bbErrorModal.open({
+                    errorDescription: errorDescription,
+                    errorTitle: errorTitle
+                });
+            }
+
             vm.onTemplateLoad = function () {
                 templateLoaded = true;
             };
 
-            vm.photoDrop = function (files) {
-                vm.bbAvatarChange({
-                    file: files[0]
-                });
+            vm.photoDrop = function (files, rejectedFiles) {
+                if (angular.isArray(rejectedFiles) && rejectedFiles.length > 0) {
+                    handleInvalidFileDrop(rejectedFiles[0]);
+                } else {
+                    vm.bbAvatarChange({
+                        file: files[0]
+                    });
+                }
             };
 
             vm.showInitials = function () {
@@ -560,6 +598,18 @@
             scope.$on('$destroy', function () {
                 revokeBlobUrl();
             });
+
+            vm.maxFileSize = bbAvatarConfig.maxFileSize;
+        }
+
+        function template(el) {
+            var dropEl;
+
+            el.html($templateCache.get('sky/templates/avatar/avatar.directive.html'));
+
+            dropEl = el.find('.bb-avatar-file-drop');
+
+            dropEl.attr('bb-file-drop-max-size', bbAvatarConfig.maxFileSize);
         }
 
         return {
@@ -572,13 +622,13 @@
             controller: angular.noop,
             controllerAs: 'bbAvatar',
             link: link,
-            templateUrl: 'sky/templates/avatar/avatar.directive.html'
+            template: template
         };
     }
 
-    bbAvatar.$inject = ['$window', 'bbPalette'];
+    bbAvatar.$inject = ['$filter', '$templateCache', '$window', 'bbAvatarConfig', 'bbErrorModal', 'bbFormat', 'bbPalette', 'bbResources'];
 
-    angular.module('sky.avatar.directive', ['sky.palette'])
+    angular.module('sky.avatar.directive', ['sky.avatar.config', 'sky.error', 'sky.format', 'sky.palette', 'sky.resources'])
         .directive('bbAvatar', bbAvatar);
 }(jQuery));
 
@@ -2480,6 +2530,54 @@
 
     components.forEach(makeErrorComponent);
 
+}());
+
+/*global angular */
+
+(function () {
+    'use strict';
+
+    function BBErrorModalController($uibModalInstance, options) {
+        var vm = this;
+
+        vm.options = options;
+
+        vm.close = function () {
+            $uibModalInstance.close();
+        };
+    }
+
+    BBErrorModalController.$inject = ['$uibModalInstance', 'options'];
+
+    angular.module('sky.errormodal.controller', ['sky.error.directive'])
+        .controller('BBErrorModalController', BBErrorModalController);
+}());
+
+/*global angular */
+
+(function () {
+    'use strict';
+
+    function bbErrorModal(bbModal) {
+        return {
+            open: function (options) {
+                return bbModal.open({
+                    controller: 'BBErrorModalController as bbErrorModal',
+                    templateUrl: 'sky/templates/error/errormodal.template.html',
+                    resolve: {
+                        options: function () {
+                            return options;
+                        }
+                    }
+                });
+            }
+        };
+    }
+
+    bbErrorModal.$inject = ['bbModal'];
+
+    angular.module('sky.errormodal.service', ['sky.errormodal.controller', 'sky.modal'])
+        .factory('bbErrorModal', bbErrorModal);
 }());
 
 /*jshint browser: true */
@@ -9000,7 +9098,7 @@ angular.module('sky.palette.config', [])
 
 var bbResourcesOverrides;
 
-bbResourcesOverrides = {"action_bar_actions":"Actions","alert_close":"Close","autonumeric_abbr_billions":"b","autonumeric_abbr_millions":"m","autonumeric_abbr_thousands":"k","checklist_select_all":"Select all","checklist_clear_all":"Clear all","checklist_no_items":"No items found","grid_back_to_top":"Back to top","grid_column_picker_all_categories":"All","grid_column_picker_description_header":"Description","grid_column_picker_header":"Choose columns to show in the list","grid_column_picker_name_header":"Column","grid_column_picker_search_placeholder":"Search by name","grid_column_picker_submit":"Apply changes","grid_columns_button":" Choose columns","grid_filters_apply":"Apply filters","grid_filters_button":"Filters","grid_filters_clear":"Clear","grid_filters_header":"Filter","grid_filters_hide":"Hide","grid_filters_summary_header":"Filter:","grid_load_more":"Load more","grid_search_placeholder":"Find in this list","grid_column_picker_search_no_columns":"No columns found","modal_footer_cancel_button":"Cancel","modal_footer_primary_button":"Save","month_short_april":"Apr","month_short_august":"Aug","month_short_december":"Dec","month_short_february":"Feb","month_short_january":"Jan","month_short_july":"Jul","month_short_june":"Jun","month_short_march":"Mar","month_short_may":"May","month_short_november":"Nov","month_short_october":"Oct","month_short_september":"Sep","page_noaccess_button":"Return to a non-classified page","page_noaccess_description":"Sorry, you don't have rights to this page.\nIf you feel you should, please contact your system administrator.","page_noaccess_header":"Move along, there's nothing to see here","text_expand_see_less":"See less","text_expand_see_more":"See more","text_expand_modal_title":"Expanded view","text_expand_close_text":"Close","grid_action_bar_clear_selection":"Clear selection","grid_action_bar_cancel_mobile_actions":"Cancel","grid_action_bar_choose_action":"Choose an action","date_field_invalid_date_message":"Please enter a valid date","date_range_picker_this_week":"This week","date_range_picker_last_week":"Last week","date_range_picker_next_week":"Next week","date_range_picker_this_month":"This month","date_range_picker_last_month":"Last month","date_range_picker_next_month":"Next month","date_range_picker_this_calendar_year":"This calendar year","date_range_picker_last_calendar_year":"Last calendar year","date_range_picker_next_calendar_year":"Next calendar year","date_range_picker_this_fiscal_year":"This fiscal year","date_range_picker_last_fiscal_year":"Last fiscal year","date_range_picker_next_fiscal_year":"Next fiscal year","date_range_picker_this_quarter":"This quarter","date_range_picker_last_quarter":"Last quarter","date_range_picker_next_quarter":"Next quarter","date_range_picker_at_any_time":"At any time","date_range_picker_today":"Today","date_range_picker_tomorrow":"Tomorrow","date_range_picker_yesterday":"Yesterday","date_range_picker_specific_range":"Specific range","date_range_picker_filter_description_this_week":"{0} for this week","date_range_picker_filter_description_last_week":"{0} from last week","date_range_picker_filter_description_next_week":"{0} for next week","date_range_picker_filter_description_this_month":"{0} for this month","date_range_picker_filter_description_last_month":"{0} from last month","date_range_picker_filter_description_next_month":"{0} for next month","date_range_picker_filter_description_this_calendar_year":"{0} for this calendar year","date_range_picker_filter_description_last_calendar_year":"{0} from last calendar year","date_range_picker_filter_description_next_calendar_year":"{0} for next calendar year","date_range_picker_filter_description_this_fiscal_year":"{0} for this fiscal year","date_range_picker_filter_description_last_fiscal_year":"{0} from last fiscal year","date_range_picker_filter_description_next_fiscal_year":"{0} for next fiscal year","date_range_picker_filter_description_this_quarter":"{0} for this quarter","date_range_picker_filter_description_last_quarter":"{0} from last quarter","date_range_picker_filter_description_next_quarter":"{0} for next quarter","date_range_picker_filter_description_at_any_time":"{0} at any time","date_range_picker_filter_description_today":"{0} for today","date_range_picker_filter_description_yesterday":"{0} from yesterday","date_range_picker_filter_description_tomorrow":"{0} for tomorrow","date_range_picker_filter_description_specific_range":"{0} from {1} to {2}","date_range_picker_from_date":"From date","date_range_picker_to_date":"To date","date_range_picker_min_date_error":"End date must be after start date","date_range_picker_max_date_error":"Start date must be before end date","file_size_b_plural":"{0} bytes","file_size_b_singular":"{0} byte","file_size_kb":"{0} KB","file_size_mb":"{0} MB","file_size_gb":"{0} GB","file_upload_drag_file_here":"Drag a file here","file_upload_drop_files_here":"Drop files here","file_upload_invalid_file":"This file type is invalid","file_upload_link_placeholder":"http://www.something.com/file","file_upload_or_click_to_browse":"or click to browse","file_upload_paste_link":"Paste a link to a file","file_upload_paste_link_done":"Done","searchfield_searching":"Searching...","searchfield_no_records":"Sorry, no matching records found","wizard_navigator_finish":"Finish","wizard_navigator_next":"Next","wizard_navigator_previous":"Previous","datepicker_today":"Today","datepicker_clear":"Clear","datepicker_close":"Done"};
+bbResourcesOverrides = {"action_bar_actions":"Actions","alert_close":"Close","autonumeric_abbr_billions":"b","autonumeric_abbr_millions":"m","autonumeric_abbr_thousands":"k","avatar_error_not_image_description":"Please choose a file that is a valid image.","avatar_error_not_image_title":"File is not an image.","avatar_error_too_large_description":"Please choose an image that is less than {0}.","avatar_error_too_large_title":"File is too large.","checklist_select_all":"Select all","checklist_clear_all":"Clear all","checklist_no_items":"No items found","grid_back_to_top":"Back to top","grid_column_picker_all_categories":"All","grid_column_picker_description_header":"Description","grid_column_picker_header":"Choose columns to show in the list","grid_column_picker_name_header":"Column","grid_column_picker_search_placeholder":"Search by name","grid_column_picker_submit":"Apply changes","grid_columns_button":" Choose columns","grid_filters_apply":"Apply filters","grid_filters_button":"Filters","grid_filters_clear":"Clear","grid_filters_header":"Filter","grid_filters_hide":"Hide","grid_filters_summary_header":"Filter:","grid_load_more":"Load more","grid_search_placeholder":"Find in this list","grid_column_picker_search_no_columns":"No columns found","modal_footer_cancel_button":"Cancel","modal_footer_primary_button":"Save","month_short_april":"Apr","month_short_august":"Aug","month_short_december":"Dec","month_short_february":"Feb","month_short_january":"Jan","month_short_july":"Jul","month_short_june":"Jun","month_short_march":"Mar","month_short_may":"May","month_short_november":"Nov","month_short_october":"Oct","month_short_september":"Sep","page_noaccess_button":"Return to a non-classified page","page_noaccess_description":"Sorry, you don't have rights to this page.\nIf you feel you should, please contact your system administrator.","page_noaccess_header":"Move along, there's nothing to see here","text_expand_see_less":"See less","text_expand_see_more":"See more","text_expand_modal_title":"Expanded view","text_expand_close_text":"Close","grid_action_bar_clear_selection":"Clear selection","grid_action_bar_cancel_mobile_actions":"Cancel","grid_action_bar_choose_action":"Choose an action","date_field_invalid_date_message":"Please enter a valid date","date_range_picker_this_week":"This week","date_range_picker_last_week":"Last week","date_range_picker_next_week":"Next week","date_range_picker_this_month":"This month","date_range_picker_last_month":"Last month","date_range_picker_next_month":"Next month","date_range_picker_this_calendar_year":"This calendar year","date_range_picker_last_calendar_year":"Last calendar year","date_range_picker_next_calendar_year":"Next calendar year","date_range_picker_this_fiscal_year":"This fiscal year","date_range_picker_last_fiscal_year":"Last fiscal year","date_range_picker_next_fiscal_year":"Next fiscal year","date_range_picker_this_quarter":"This quarter","date_range_picker_last_quarter":"Last quarter","date_range_picker_next_quarter":"Next quarter","date_range_picker_at_any_time":"At any time","date_range_picker_today":"Today","date_range_picker_tomorrow":"Tomorrow","date_range_picker_yesterday":"Yesterday","date_range_picker_specific_range":"Specific range","date_range_picker_filter_description_this_week":"{0} for this week","date_range_picker_filter_description_last_week":"{0} from last week","date_range_picker_filter_description_next_week":"{0} for next week","date_range_picker_filter_description_this_month":"{0} for this month","date_range_picker_filter_description_last_month":"{0} from last month","date_range_picker_filter_description_next_month":"{0} for next month","date_range_picker_filter_description_this_calendar_year":"{0} for this calendar year","date_range_picker_filter_description_last_calendar_year":"{0} from last calendar year","date_range_picker_filter_description_next_calendar_year":"{0} for next calendar year","date_range_picker_filter_description_this_fiscal_year":"{0} for this fiscal year","date_range_picker_filter_description_last_fiscal_year":"{0} from last fiscal year","date_range_picker_filter_description_next_fiscal_year":"{0} for next fiscal year","date_range_picker_filter_description_this_quarter":"{0} for this quarter","date_range_picker_filter_description_last_quarter":"{0} from last quarter","date_range_picker_filter_description_next_quarter":"{0} for next quarter","date_range_picker_filter_description_at_any_time":"{0} at any time","date_range_picker_filter_description_today":"{0} for today","date_range_picker_filter_description_yesterday":"{0} from yesterday","date_range_picker_filter_description_tomorrow":"{0} for tomorrow","date_range_picker_filter_description_specific_range":"{0} from {1} to {2}","date_range_picker_from_date":"From date","date_range_picker_to_date":"To date","date_range_picker_min_date_error":"End date must be after start date","date_range_picker_max_date_error":"Start date must be before end date","errormodal_ok":"OK","file_size_b_plural":"{0} bytes","file_size_b_singular":"{0} byte","file_size_kb":"{0} KB","file_size_mb":"{0} MB","file_size_gb":"{0} GB","file_upload_drag_file_here":"Drag a file here","file_upload_drop_files_here":"Drop files here","file_upload_invalid_file":"This file type is invalid","file_upload_link_placeholder":"http://www.something.com/file","file_upload_or_click_to_browse":"or click to browse","file_upload_paste_link":"Paste a link to a file","file_upload_paste_link_done":"Done","searchfield_searching":"Searching...","searchfield_no_records":"Sorry, no matching records found","wizard_navigator_finish":"Finish","wizard_navigator_next":"Next","wizard_navigator_previous":"Previous","datepicker_today":"Today","datepicker_clear":"Clear","datepicker_close":"Done"};
 
 angular.module('sky.resources')
     .config(['bbResources', function (bbResources) {
@@ -9050,6 +9148,7 @@ angular.module('sky.templates', []).run(['$templateCache', function($templateCac
         '<div class="bb-avatar" ng-switch="bbAvatar.canChange">\n' +
         '  <div ng-switch-when="true">\n' +
         '    <div\n' +
+        '       class="bb-avatar-file-drop"\n' +
         '       bb-file-drop\n' +
         '       bb-file-drop-change="bbAvatar.photoDrop(files, rejectedFiles)"\n' +
         '       bb-file-drop-accept="\'image/*\'"\n' +
@@ -9225,6 +9324,25 @@ angular.module('sky.templates', []).run(['$templateCache', function($templateCac
         '    <ng-transclude></ng-transclude>\n' +
         '  </section>\n' +
         '</div>\n' +
+        '');
+    $templateCache.put('sky/templates/error/errormodal.template.html',
+        '<bb-modal>\n' +
+        '  <div class="modal-form">\n' +
+        '    <div bb-modal-body>\n' +
+        '      <bb-error>\n' +
+        '        <bb-error-title>\n' +
+        '          {{bbErrorModal.options.errorTitle}}\n' +
+        '        </bb-error-title>\n' +
+        '        <bb-error-description>\n' +
+        '          {{bbErrorModal.options.errorDescription}}\n' +
+        '        </bb-error-description>\n' +
+        '        <bb-error-action>\n' +
+        '          <button type="button" class="btn btn-primary" ng-click="bbErrorModal.close()">{{\'errormodal_ok\' | bbResources}}</button>\n' +
+        '        </bb-error-action>\n' +
+        '      </bb-error>\n' +
+        '    </div>\n' +
+        '  </div>\n' +
+        '</bb-modal>\n' +
         '');
     $templateCache.put('sky/templates/fileattachments/filedrop.html',
         '<div class="row bb-file-drop-row">\n' +
