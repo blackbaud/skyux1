5d09dc6539f6fd27fc0327b9bb5596b3757d34ab
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-02-29 16:22:05 -0500

    last visual test updates
    type, wizard, wait

--- webdrivertest/test/wizard/wizard.visual.js
+++ webdrivertest/test/wizard/wizard.visual.js
@@ -1,29 +1,29 @@
 
-/*global describe, it, browser, beforeEach, expect, require */
+/*global describe, it, browser, beforeEach, require */
 
 describe('wizard', function () {
     'use strict';
 
-    var options = {};
+    var options = {},
+        common;
 
     beforeEach(function (done) {
-        require('../common').initWebdriverCss(browser, options, done);
+        common = require('../common');
+        common.initWebdriverCss(browser, options, done);
     });
 
-    it('should take the wizard screenshot', function (done) {
-        var screenshotName = 'wizard',
-            pageName = options.prefix + screenshotName + '_full';
-        browser
-            .url('/wizard/fixtures/test.full.html')
-            .click('#screenshots-wizard-open')
-            .webdrivercss(pageName, [
-                {
-                    name: screenshotName,
-                    elem: '.modal-content'
-                }
-            ], function (err, res) {
-                expect(err).toBe(undefined);
-                expect(res[screenshotName][0].isWithinMisMatchTolerance).toBe(true);
-            }).call(done);
+    it('should match the baseline wizard screenshot', function (done) {
+        var result;
+
+        result = browser.url('/wizard/fixtures/test.full.html')
+            .click('#screenshots-wizard-open');
+
+        common.compareScreenshot({
+            browserResult: result,
+            prefix: options.prefix,
+            screenshotName: 'wizard',
+            selector: '.modal-content',
+            done: done
+        });
     });
 });
