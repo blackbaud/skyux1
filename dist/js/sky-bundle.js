f84094bc5c9db46610b1893b8669e8db1ac2ee48
Travis <travis@travis-ci.org>
2016-03-01 00:13:03 +0000

    Travis build 556 pushed to skyux [ci skip]

--- dist/js/sky-bundle.js
+++ dist/js/sky-bundle.js
@@ -107702,16 +107702,6 @@ angular.module('sky.palette.config', [])
                         }
 
                         scope.isCollapsed = collapsed;
-
-                        if (collapsed && !tileInitialized) {
-                            //in some cases the tile-content div is left in a partially collapsed state.
-                            //   this will ensure that the tile is styled corretly and the tile is completely collapsed
-                            $timeout(function () {
-                                var contentEl;
-                                contentEl = el.find('.bb-tile-content');
-                                contentEl.removeClass('collapsing').addClass('collapse');
-                            }, 1);
-                        }
                     }
 
                     function updateHeaderContent() {
