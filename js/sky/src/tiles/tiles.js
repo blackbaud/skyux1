d6e5eeab8773c30655d95f6488c3fa9a8f7ac8bc
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-03-03 10:35:33 -0500

    Use angular animation for tile header and make all tile directives use bind to controller

--- js/sky/src/tiles/tiles.js
+++ js/sky/src/tiles/tiles.js
@@ -168,7 +168,7 @@
 
             //If the dashboard has already been initialized and this tile hasn't, initialize tile.
             if (dashboardCtrl !== null) {
-                if (dashboardCtrl.dashboardInitialized() && !vm.tileInitialized) {
+                if (dashboardCtrl.dashboardInitialized && !vm.tileInitialized) {
                     dashboardState = dashboardCtrl.getDashboardState();
                     initializeTile(dashboardState);
                     dashboardCtrl.layoutTiles();
@@ -193,217 +193,240 @@
         };
     }
 
+    function bbTileHeaderContent() {
+        return {
+            replace: true,
+            require: '^bbTile',
+            restrict: 'E',
+            link: function (scope, el, attrs, tileCtrl) {
+                tileCtrl.setHeaderContentEl(el);
+            },
+            templateUrl: 'sky/templates/tiles/tileheadercontent.html',
+            transclude: true
+        };
+    }
+
+    function bbTileHeaderCheck() {
+        return {
+            replace: true,
+            require: '^bbTileHeaderContent',
+            restrict: 'E',
+            templateUrl: 'sky/templates/tiles/tileheadercheck.html'
+        };
+    }
+
+    function bbTileSection() {
+        return {
+            restrict: 'A',
+            template: function (el) {
+                el.addClass('bb-tile-content-section');
+            }
+        };
+    }
+
+    function BBTileDashboardController($scope, $timeout) {
+        var vm = this;
+
+        function fireDisplayModeChanged() {
+            $scope.$broadcast('tileDisplayModeChanged', {
+                smallTileDisplayMode: vm.smallTileDisplayMode,
+                tiles: vm.tiles
+            });
+        }
+
+        vm.getDashboardState = function () {
+            return {tiles: vm.tiles, smallTileDisplayMode: vm.smallTileDisplayMode};
+        };
+
+        vm.layoutTiles = function () {
+            /* This timeout is in place to allow a state change to
+               complete before laying out tiles
+            */
+            $timeout(function () {
+                vm.layoutTileColumns();
+            });
+        };
+
+        vm.dashboardInitialized = false;
+        vm.smallTileDisplayMode = false;
+
+        vm.fireDisplayModeChanged = fireDisplayModeChanged;
+
+        $scope.$watch(function () {
+            return vm.tiles;
+        }, function () {
+            $timeout(function () {
+                vm.layoutTileColumns();
+                $scope.$broadcast('tilesInitialized', {
+                    smallTileDisplayMode: vm.smallTileDisplayMode,
+                    tiles: vm.tiles
+                });
+
+                vm.dashboardInitialized = true;
+            });
+        });
+
+        $scope.$watch(function () {
+            return vm.allCollapsed;
+        }, function (newValue) {
+            var i,
+                n,
+                tiles = vm.tiles;
+
+            // Check for an explicit true/false here since null/undefined is the
+            // indeterminate state.
+            if (newValue === true || newValue === false) {
+                for (i = 0, n = tiles.length; i < n; i++) {
+                    if (vm.smallTileDisplayMode) {
+                        tiles[i].collapsed_small = newValue;
+                    } else {
+                        tiles[i].collapsed = newValue;
+                    }
+                }
+
+                vm.fireDisplayModeChanged();
+            }
+        });
+    }
+
+    BBTileDashboardController.$inject = ['$scope', '$timeout'];
+
+    function bbTileDashboard($timeout, bbMediaBreakpoints) {
+
+        function link($scope, element, attrs, vm) {
+            var column1 = element.find('[data-dashboard-column="1"]'),
+                column2 = element.find('[data-dashboard-column="2"]'),
+                singleColumnMode = false,
+                sortableOptions;
+
+            //Layouts out the tiles based on the current one column or two column configuration
+            function layoutTileColumns() {
+                var layout = vm.layout;
+
+                if (layout) {
+                    if (singleColumnMode) {
+                        moveTilesToContainer(element, column1, layout.one_column_layout);
+                    } else {
+                        moveTilesToContainer(element, column1, layout.two_column_layout[0]);
+                        moveTilesToContainer(element, column2, layout.two_column_layout[1]);
+                    }
+                }
+            }
+
+            vm.layoutTileColumns = layoutTileColumns;
+
+
+            //Inspects the tiles in each column and updates model accordingly.
+            function parseColumnTiles() {
+                $scope.$apply(function () {
+                    var layout = vm.layout;
+
+                    if (singleColumnMode) {
+                        layout.one_column_layout = parseTileOrder(column1);
+                    } else {
+                        layout.two_column_layout[0] = parseTileOrder(column1);
+                        layout.two_column_layout[1] = parseTileOrder(column2);
+                    }
+                });
+            }
+
+            function mediabreakpointChangeHandler(breakPoints) {
+                singleColumnMode = (breakPoints.xs || breakPoints.sm);
+                vm.layoutTileColumns();
+
+                if (singleColumnMode) {
+                    element.removeClass('bb-page-content-multicolumn');
+                    column2.hide();
+                } else {
+                    element.addClass('bb-page-content-multicolumn');
+                    column2.show();
+                }
+
+                vm.smallTileDisplayMode = breakPoints.xs;
+
+                vm.fireDisplayModeChanged();
+            }
+
+            //Setup jQuery sortable (drag and drop) options for the dashboard columns
+            sortableOptions = {
+                connectWith: '[data-dashboard-column]',
+                update: parseColumnTiles,
+                opacity: 0.8,
+                handle: '.bb-tile-grab-handle',
+                placeholder: 'placeholder bb-tile',
+                forcePlaceholderSize: true,
+                revert: 250
+            };
+
+            //Setup jQuery sortable drag/drop for the columns
+            column1.sortable(sortableOptions);
+            column2.sortable(sortableOptions);
+
+            bbMediaBreakpoints.register(mediabreakpointChangeHandler);
+
+            element.on('$destroy', function () {
+                bbMediaBreakpoints.unregister(mediabreakpointChangeHandler);
+            });
+
+            $scope.$on('tileStateChanged', function (event, data) {
+                /*jslint unparam: true */
+                $scope.$apply(function () {
+                    var allCollapsed = null,
+                        collapsed,
+                        collapsedProp,
+                        i,
+                        n,
+                        tile,
+                        tileId = data.tileId || /*istanbul ignore next: default value */ '',
+                        tiles = vm.tiles;
+
+                    collapsed = data.collapsed || false;
+                    collapsedProp = vm.smallTileDisplayMode ? 'collapsed_small' : 'collapsed';
+
+                    for (i = 0, n = tiles.length; i < n; i++) {
+                        tile = tiles[i];
+
+                        if (tile.id === tileId) {
+                            tile[collapsedProp] = collapsed;
+                        }
+
+                        if (i > 0 && tile[collapsedProp] !== allCollapsed) {
+                            allCollapsed = null;
+                        } else {
+                            allCollapsed = tile[collapsedProp];
+                        }
+                    }
+
+                    if (attrs.bbTileDashboardAllCollapsed) {
+                        vm.allCollapsed = allCollapsed;
+                    }
+                });
+            });
+        }
+
+        return {
+            replace: true,
+            require: 'bbTileDashboard',
+            restrict: 'E',
+            bindToController: {
+                tiles: '=bbTiles',
+                layout: '=bbLayout',
+                allCollapsed: '=bbTileDashboardAllCollapsed'
+            },
+            scope: {},
+            link: link,
+            controller: BBTileDashboardController,
+            controllerAs: 'bbTileDashboard',
+            templateUrl: 'sky/templates/tiles/tiledashboard.html'
+        };
+    }
+
+    bbTileDashboard.$inject = ['$timeout', 'bbMediaBreakpoints'];
+
     angular.module('sky.tiles', ['sky.mediabreakpoints'])
         .directive('bbTile', bbTile)
-        .directive('bbTileHeaderContent', function () {
-            return {
-                replace: true,
-                require: '^bbTile',
-                restrict: 'E',
-                link: function (scope, el, attrs, tileCtrl) {
-                    tileCtrl.setHeaderContentEl(el);
-                },
-                templateUrl: 'sky/templates/tiles/tileheadercontent.html',
-                transclude: true
-            };
-        })
-        .directive('bbTileHeaderCheck', function () {
-            return {
-                replace: true,
-                require: '^bbTileHeaderContent',
-                restrict: 'E',
-                templateUrl: 'sky/templates/tiles/tileheadercheck.html'
-            };
-        })
-        .directive('bbTileSection', function () {
-            return {
-                restrict: 'A',
-                template: function (el) {
-                    el.addClass('bb-tile-content-section');
-                }
-            };
-        })
-        .directive('bbTileDashboard', ['$timeout', 'bbMediaBreakpoints', function ($timeout, bbMediaBreakpoints) {
-            return {
-                replace: true,
-                restrict: 'E',
-                scope: {
-                    tiles: '=bbTiles',
-                    layout: '=bbLayout',
-                    allCollapsed: '=bbTileDashboardAllCollapsed'
-                },
-                link: function (scope, element, attrs) {
-                    var column1 = element.find('[data-dashboard-column="1"]'),
-                        column2 = element.find('[data-dashboard-column="2"]'),
-                        singleColumnMode = false,
-                        sortableOptions;
-
-                    scope.dashboardInitialized = false;
-                    scope.smallTileDisplayMode = false;
-
-                    //Inspects the tiles in each column and updates model accordingly.
-                    function parseColumnTiles() {
-                        scope.$apply(function () {
-                            var layout = scope.layout;
-
-                            if (singleColumnMode) {
-                                layout.one_column_layout = parseTileOrder(column1);
-                            } else {
-                                layout.two_column_layout[0] = parseTileOrder(column1);
-                                layout.two_column_layout[1] = parseTileOrder(column2);
-                            }
-                        });
-                    }
-
-                    //Layouts out the tiles based on the current one column or two column configuration
-                    function layoutTiles() {
-                        var layout = scope.layout;
-
-                        if (layout) {
-                            if (singleColumnMode) {
-                                moveTilesToContainer(element, column1, layout.one_column_layout);
-                            } else {
-                                moveTilesToContainer(element, column1, layout.two_column_layout[0]);
-                                moveTilesToContainer(element, column2, layout.two_column_layout[1]);
-                            }
-                        }
-                    }
-
-                    function fireDisplayModeChanged() {
-                        scope.$broadcast('tileDisplayModeChanged', {
-                            smallTileDisplayMode: scope.smallTileDisplayMode,
-                            tiles: scope.tiles
-                        });
-                    }
-
-                    function mediabreakpointChangeHandler(breakPoints) {
-                        singleColumnMode = (breakPoints.xs || breakPoints.sm);
-                        layoutTiles();
-
-                        if (singleColumnMode) {
-                            element.removeClass('bb-page-content-multicolumn');
-                            column2.hide();
-                        } else {
-                            element.addClass('bb-page-content-multicolumn');
-                            column2.show();
-                        }
-
-                        scope.smallTileDisplayMode = breakPoints.xs;
-
-                        fireDisplayModeChanged();
-                    }
-
-                    //Setup jQuery sortable (drag and drop) options for the dashboard columns
-                    sortableOptions = {
-                        connectWith: '[data-dashboard-column]',
-                        update: parseColumnTiles,
-                        opacity: 0.8,
-                        handle: '.bb-tile-grab-handle',
-                        placeholder: 'placeholder bb-tile',
-                        forcePlaceholderSize: true,
-                        revert: 250
-                    };
-
-                    //Setup jQuery sortable drag/drop for the columns
-                    column1.sortable(sortableOptions);
-                    column2.sortable(sortableOptions);
-
-                    bbMediaBreakpoints.register(mediabreakpointChangeHandler);
-
-                    scope.layoutTiles = layoutTiles;
-
-                    element.on('$destroy', function () {
-                        bbMediaBreakpoints.unregister(mediabreakpointChangeHandler);
-                    });
-
-                    scope.$watch('tiles', function () {
-                        $timeout(function () {
-                            layoutTiles();
-                            scope.$broadcast('tilesInitialized', {
-                                smallTileDisplayMode: scope.smallTileDisplayMode,
-                                tiles: scope.tiles
-                            });
-
-                            scope.dashboardInitialized = true;
-                        });
-                    });
-
-                    scope.$watch('allCollapsed', function (newValue) {
-                        var i,
-                            n,
-                            tiles = scope.tiles;
-
-                        // Check for an explicit true/false here since null/undefined is the
-                        // indeterminate state.
-                        if (newValue === true || newValue === false) {
-                            for (i = 0, n = tiles.length; i < n; i++) {
-                                if (scope.smallTileDisplayMode) {
-                                    tiles[i].collapsed_small = newValue;
-                                } else {
-                                    tiles[i].collapsed = newValue;
-                                }
-                            }
-
-                            fireDisplayModeChanged();
-                        }
-                    });
-
-                    scope.$on('tileStateChanged', function (event, data) {
-                        /*jslint unparam: true */
-                        scope.$apply(function () {
-                            var allCollapsed = null,
-                                collapsed,
-                                collapsedProp,
-                                i,
-                                n,
-                                tile,
-                                tileId = data.tileId || /*istanbul ignore next: default value */ '',
-                                tiles = scope.tiles;
-
-                            collapsed = data.collapsed || false;
-                            collapsedProp = scope.smallTileDisplayMode ? 'collapsed_small' : 'collapsed';
-
-                            for (i = 0, n = tiles.length; i < n; i++) {
-                                tile = tiles[i];
-
-                                if (tile.id === tileId) {
-                                    tile[collapsedProp] = collapsed;
-                                }
-
-                                if (i > 0 && tile[collapsedProp] !== allCollapsed) {
-                                    allCollapsed = null;
-                                } else {
-                                    allCollapsed = tile[collapsedProp];
-                                }
-                            }
-
-                            if (attrs.bbTileDashboardAllCollapsed) {
-                                scope.allCollapsed = allCollapsed;
-                            }
-                        });
-                    });
-                },
-                controller: ['$scope', function ($scope) {
-                    var self = this;
-
-                    self.getDashboardState = function () {
-                        return {tiles: $scope.tiles, smallTileDisplayMode: $scope.smallTileDisplayMode};
-                    };
-
-                    self.dashboardInitialized = function () {
-                        return $scope.dashboardInitialized;
-                    };
-
-                    self.layoutTiles = function () {
-                        /* This timeout is in place to allow a state change to
-                           complete before laying out tiles
-                        */
-                        $timeout(function () {
-                            $scope.layoutTiles();
-                        });
-                    };
-                }],
-                templateUrl: 'sky/templates/tiles/tiledashboard.html'
-            };
-        }]);
+        .directive('bbTileHeaderContent', bbTileHeaderContent)
+        .directive('bbTileHeaderCheck', bbTileHeaderCheck)
+        .directive('bbTileSection', bbTileSection)
+        .directive('bbTileDashboard', bbTileDashboard);
 }());
