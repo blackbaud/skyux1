a4982a53e8de6d0dd7fa3872b64b773f63e9a8cd
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-03-03 11:46:11 -0500

    use additional browserstack local flag and remove chrome specific version

--- run-browserstack-local.sh
+++ run-browserstack-local.sh
@@ -1,4 +1,4 @@
 wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
 unzip BrowserStackLocal-linux-x64.zip
-./BrowserStackLocal -v -onlyAutomate -forcelocal $BROWSER_STACK_ACCESS_KEY &
+./BrowserStackLocal -v -onlyAutomate -forcelocal -f $BROWSER_STACK_ACCESS_KEY &
 sleep 3
