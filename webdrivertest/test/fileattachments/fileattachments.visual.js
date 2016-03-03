792bc254ed0e87c8ee50ed9a04b5a75bbc99a467
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-03-03 13:55:47 -0500

    Revert "Revert "update test names and use common classes #354""

--- webdrivertest/test/fileattachments/fileattachments.visual.js
+++ webdrivertest/test/fileattachments/fileattachments.visual.js
@@ -1,28 +1,29 @@
-/*global describe, it, browser, beforeEach, expect, require */
+/*global describe, it, browser, beforeEach, require */
 
 describe('file attachments', function () {
     'use strict';
 
-    var options = {};
+    var options = {},
+        common;
 
     beforeEach(function (done) {
-        require('../common').initWebdriverCss(browser, options, done);
+        common = require('../common');
+        common.initWebdriverCss(browser, options, done);
     });
 
 
-    it('should take fileattachment screenshots', function (done) {
-        var screenshotName = 'fileattachments',
-            pageName = options.prefix + screenshotName + '_full';
-        browser
-            .url('/fileattachments/fixtures/test.full.html')
-            .webdrivercss(pageName, [
-                {
-                    name: screenshotName,
-                    elem: '#screenshot-fileattachments'
-                }
-            ], function (err, res) {
-                expect(err).toBe(undefined);
-                expect(res[screenshotName][0].isWithinMisMatchTolerance).toBe(true);
-            }).call(done);
+    it('should match the baseline fileattachment screenshot', function (done) {
+        var result;
+
+        result = browser.url('/fileattachments/fixtures/test.full.html');
+
+        common.compareScreenshot({
+            browserResult: result,
+            prefix: options.prefix,
+            screenshotName: 'fileattachments',
+            selector: '#screenshot-fileattachments',
+            done: done
+        });
+
     });
 });
--- webdrivertest/test/tabs/tabs.visual.js
+++ webdrivertest/test/tabs/tabs.visual.js
@@ -1,30 +1,29 @@
-
-/*global describe, it, browser, beforeEach, expect, require */
+/*global describe, it, browser, beforeEach, require */
 
 describe('tabs', function () {
     'use strict';
 
-    var options = {};
+    var options = {},
+        common;
 
     beforeEach(function (done) {
-        require('../common').initWebdriverCss(browser, options, done);
+        common = require('../common');
+        common.initWebdriverCss(browser, options, done);
     });
 
 
-    it('should take tab screenshots', function (done) {
-        var screenshotName = 'tabs',
-            pageName = options.prefix + screenshotName + '_full';
-        browser
-            .url('/tabs/fixtures/test.full.html')
-            .moveToObject('#screenshot-tab-2')
-            .webdrivercss(pageName, [
-                {
-                    name: screenshotName,
-                    elem: '#screenshot-tabs'
-                }
-            ], function (err, res) {
-                expect(err).toBe(undefined);
-                expect(res[screenshotName][0].isWithinMisMatchTolerance).toBe(true);
-            }).call(done);
+    it('should match the baseline tab screenshot', function (done) {
+        var result;
+
+        result = browser.url('/tabs/fixtures/test.full.html')
+            .moveToObject('#screenshot-tab-2');
+
+        common.compareScreenshot({
+            browserResult: result,
+            prefix: options.prefix,
+            screenshotName: 'tabs',
+            selector: '#screenshot-tabs',
+            done: done
+        });
     });
 });
