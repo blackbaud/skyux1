a13b243bb154b8895af9050b636936cda0b7cd85
Bobby Earl <bobby.earl@blackbaud.com>
2016-03-08 15:29:38 -0500

    More logging

--- scripts/browserstack-local-stop.sh
+++ scripts/browserstack-local-stop.sh
@@ -1,5 +1,8 @@
 # Kill any remaining BrowserStack tunnels
-echo "Killing any rogue BrowserStack tunnels."
+echo "Cleaning up BrowerStack instances"
+echo "--- Before ---"
 ps -ef | grep BrowserStackLocal
 pkill BrowserStackLocal
+
+echo "--- After ---"
 ps -ef | grep BrowserStackLocal
