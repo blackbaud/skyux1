5b1897a9f1a9adf161cd5ae3e92e42a1f66a1ecc
Paul Crowder <paul.crowder@blackbaud.com>
2016-04-04 10:27:53 -0400

    More left/right header content work

--- js/sky/src/card/docs/demo.js
+++ js/sky/src/card/docs/demo.js
@@ -3,16 +3,27 @@
 (function () {
     'use strict';
 
-    function CardTestController() {
+    function CardTestController($scope) {
         var vm = this;
 
         vm.showTitle = true;
+        vm.showHeaderLeft = true;
+        vm.showHeaderRight = true;
         vm.showContent = true;
         vm.showActions = true;
         vm.showCheckbox = true;
+
+        $scope.$watch(function () {
+            return vm.showCheckbox;
+        }, function () {
+            if (vm.showCheckbox) {
+                vm.showHeaderRight = false;
+            }
+        });
     }
+
+    CardTestController.$inject = ['$scope'];
 
     angular.module('stache')
         .controller('CardTestController', CardTestController);
 }());
- 
--- js/sky/src/card/docs/demo.md
+++ js/sky/src/card/docs/demo.md
@@ -1,9 +1,9 @@
 ---
 name: Card
-icon: mobile
-summary: The check applies a commonly styled selector to a checkbox or radio button.
+icon: square-o
+summary: Card card card
 ---
 
-The `bb-check` directive allows you to change an input element of type checkbox or radio button into a commonly styled selector. The value that is selected is driven through the `ng-model` attribute specified on the input element. For radio button input types, the value to set on the `ng-model` can be specified by the value attribute.
+Card card card, card card card card.
 
 ---
