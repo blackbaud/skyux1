548a7694c94cdd5fe5f2ad9974b076a539842c29
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-04-05 08:56:04 -0400

    fix to running browserstack locally

--- config/grunt/test.js
+++ config/grunt/test.js
@@ -10,7 +10,7 @@ module.exports = function (grunt, env, utils) {
                     'config/grunt/*.js',
                     'js/**/*.js'
                 ],
-                webdriver: 'webdriver-screenshots' + (env.isCurrent(env.SUPPORTED.LOCAL) ? 'local' : '')
+                webdriver: 'webdriver-screenshots' + (env.isCurrent(env.SUPPORTED.LOCAL) || env.isCurrent(env.SUPPORTED.LOCAL_BS) ? 'local' : '')
             }
         },
         connect: {
