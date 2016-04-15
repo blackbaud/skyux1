fd33d3d7a991bffb532f8a019a593ba74a2aa6ab
Paul Crowder <paul.crowder@blackbaud.com>
2016-04-15 10:27:42 -0400

    Repeater component (#497)
    * Initial work on repeater component
    
    * Chevron labels; correct chevron direction
    
    * Created repeater spec file
    
    * Repeater component unit tests
    
    * Visual regression tests
    
    * Flipped "collapsed" attribute to "expanded"
    
    * Changed expand mode to attribute binding; fixed code coverage
    
    * Fixed expand mode in docs and visual regression tests
    
    * Moved repeater item chevron to its own component so it can be reused
    
    * Convert initial repeater docs from Todd into markdown file and flesh out topic based on the demo (#490)
    
    * Documentation tweak and typo fix
    
    * Tweaks to padding and demo

---
+++ js/sky/src/repeater/docs/demo.js
@@ -0,0 +1,46 @@
+/*global angular */
+
+(function () {
+    'use strict';
+
+    function RepeaterTestController() {
+        var vm = this;
+
+        vm.expandMode = 'none';
+        vm.showContextMenu = true;
+        vm.showTitle = true;
+
+        vm.items = [
+            {
+                title: 'Call Robert Hernandez',
+                note: 'Robert recently gave a very generous gift.  We should call him to thank him.',
+                status: 'Completed',
+                statusType: 'success'
+            },
+            {
+                title: 'Send invitation to Spring Ball',
+                note: 'The Spring Ball is coming up soon.  Let\'s get those invitations out!',
+                status: 'Past due',
+                statusType: 'warning'
+            }
+        ];
+
+        vm.addItem = function () {
+            var next = vm.items.length + 1;
+            vm.items.push({
+                title: 'New reminder ' + next,
+                note: 'This is a new reminder',
+                expanded: true,
+                status: 'Active',
+                statusType: 'info'
+            });
+        }
+
+        vm.getStatusCls = function (item) {
+            return 'label-' + item.statusType;
+        }
+    }
+
+    angular.module('stache')
+        .controller('RepeaterTestController', RepeaterTestController);
+}());
