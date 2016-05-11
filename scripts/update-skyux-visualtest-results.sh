4687746de09502d278ce7e627d2486b2c4ed0c17
Bobby Earl <bobby.earl@blackbaud.com>
2016-05-11 11:54:53 -0400

    Consistently using sky-build-user.  Added support for savage branches to update-skyux-visualtest-results.sh

--- scripts/update-skyux-visualtest-results.sh
+++ scripts/update-skyux-visualtest-results.sh
@@ -5,13 +5,17 @@ if [[ "$(ls -A webdriver-screenshots-diffs)" ]]; then
 
     echo -e "Starting to update webdriver test results.\n"
 
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "Travis"
+    git config --global user.email "sky-build-user@blackbaud.com"
+    git config --global user.name "Blackbaud Sky Build User"
     git clone --quiet https://${GH_TOKEN}@github.com/blackbaud/skyux-visualtest-results.git skyux-visualtest-results-webdriver > /dev/null
 
     cd skyux-visualtest-results-webdriver
 
-    git checkout -b ${TRAVIS_BUILD_NUMBER}-webdriver
+    branch="$TRAVIS_BUILD_NUMBER-webdriver"
+    if [[ $TRAVIS_BRANCH =~ $SAVAGE_BRANCH ]]; then
+      branch="$TRAVIS_BUILD_NUMBER-webdriver-savage"
+    fi
+    git checkout -b $branch
 
     mkdir -p failures
 
@@ -26,7 +30,7 @@ if [[ "$(ls -A webdriver-screenshots-diffs)" ]]; then
         echo -e "No changes to commit to skyux visual test webdriver results."
     else
         git commit -m "Travis build $TRAVIS_BUILD_NUMBER webdriver screenshot results pushed to skyux-visualtest-results"
-        git push -fq origin ${TRAVIS_BUILD_NUMBER}-webdriver > /dev/null
-        echo -e "skyux-visualtest-results webdriver successfully updated.\nTest results may be viewed at https://github.com/blackbaud/skyux-visualtest-results"
+        git push -fq origin $branch > /dev/null
+        echo -e "skyux-visualtest-results webdriver successfully updated.\nTest results may be viewed at https://github.com/blackbaud/skyux-visualtest-results/tree/$branch"
     fi
 fi
