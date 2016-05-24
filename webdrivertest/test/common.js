009619a3f002896ad2d9fdd14ce78bfe719e628a
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-05-24 16:02:57 -0400

    Add accessibility automation for actionbar #528

--- webdrivertest/test/common.js
+++ webdrivertest/test/common.js
@@ -1,5 +1,5 @@
 /*jshint jasmine: true */
-/* global module */
+/* global module, axe, document, console */
 
 (function () {
     'use strict';
@@ -13,6 +13,31 @@
         }
 
         return platform + '_' + browserName;
+    }
+
+    function checkAccessibility(options) {
+        options.browserResult.executeAsync(function (done) {
+            axe.a11yCheck(document, function (results) {
+                done(results);
+            });
+        }).then(function (ret) {
+            var i,
+                j,
+                violation;
+            if (ret.value.violations && ret.value.violations.length !== 0) {
+                console.log('\nThe following accessibility issues exist in ' + options.screenshotName + ': \n');
+                for (i = 0; i < ret.value.violations.length; i++) {
+                    violation = ret.value.violations[i];
+                    console.log(violation.help);
+                    console.log('violation at: ');
+                    for (j = 0; j < violation.nodes.length; j++) {
+                        console.log(violation.nodes[j].target);
+                    }
+                    console.log('See: ' + violation.helpUrl + '\n');
+                }
+                expect(ret.value.violations.length).toBe(0, ' number of accesiblity violations');
+            }
+        }).call(options.done);
     }
 
     module.exports = {
@@ -30,8 +55,16 @@
                 ], function (err, res) {
                     expect(err).toBe(undefined);
                     expect(res[options.screenshotName][0].isWithinMisMatchTolerance).toBe(true);
-                }).call(options.done);
+                })
+                .then(function () {
+                    if (options.checkAccessibility) {
+                        checkAccessibility(options);
+                    } else {
+                        options.done();
+                    }
+                });
         },
+        checkAccessibility: checkAccessibility,
         moveCursorOffScreen: function (browser) {
             return browser.moveToObject('body', 0, 0);
         }
