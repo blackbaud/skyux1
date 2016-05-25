1b80ea35358877c597cb921aa4d9bf06849a6a5d
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-05-25 17:32:29 -0400

    make tabsref demo respond to url

--- js/sky/src/tabsref/docs/demo.js
+++ js/sky/src/tabsref/docs/demo.js
@@ -4,16 +4,24 @@
 
     function TabSrefConfig($stateProvider) {
         $stateProvider
+            .state('rootState', {
+                url: '/',
+                template: '<uib-tabset><uib-tab heading="Tab 1" bb-tab-sref="TabState1">Content</uib-tab><uib-tab heading="Tab 2" bb-tab-sref="TabState2">Content 2</uib-tab><uib-tab heading="Tab 3" bb-tab-sref="TabState3">Content 3</uib-tab></uib-tabset>'
+            })
             .state('TabState1', {
-                url: 'tabsref'
+                parent: 'rootState',
+                url: 'tabsref/tab1'
             })
             .state('TabState2', {
+                parent: 'rootState',
                 url: 'tabsref/tab2'
             })
             .state('TabState3', {
+                parent: 'rootState',
                 url: 'tabsref/tab3'
             });
     }
+
 
     TabSrefConfig.$inject = ['$stateProvider'];
 
