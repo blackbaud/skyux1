82c500e65d5657796f14413218d543f9be2e387e
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-06-06 13:02:05 -0400

    Update page demo and documentation for bbPage #599
    Includes example of not found state, bbPageConfig options, and change
    to bb-page-uses-load-manager wording

--- js/sky/src/page/docs/demo.js
+++ js/sky/src/page/docs/demo.js
@@ -2,38 +2,61 @@
 (function () {
     'use strict';
 
-    function PageTestController($scope, $timeout, bbPage) {
+    function PageTestController($scope, $timeout, bbPage, $location) {
         var self = this,
             testLoading = false;
 
         function simulateLoading() {
             testLoading = true;
             self.pageStatus = bbPage.pageStatuses.LOADING;
-            $scope.$emit('bbBeginWait');
 
             $timeout(function () {
                 testLoading = false;
                 self.pageStatus = bbPage.pageStatuses.LOADED;
-                $scope.$emit('bbEndWait');
             }, 1500);
         }
 
         function simulateNotAuthorized() {
             self.pageStatus = bbPage.pageStatuses.NOT_AUTHORIZED;
         }
-
-        $scope.$on('bbBeginWait', function (event) {
-            if (!testLoading) {
-                event.stopPropagation();
-            }
-        });
+        
+        function simulateNotFound() {
+            self.pageStatus = bbPage.pageStatuses.NOT_FOUND;
+        }
+        
+        function returnHome() {
+            simulateLoading();
+            $location.path('/').replace();
+        }
 
         self.pageStatus = bbPage.pageStatuses.LOADED;
         self.simulateLoading = simulateLoading;
         self.simulateNotAuthorized = simulateNotAuthorized;
+        self.simulateNotFound = simulateNotFound;
+        self.returnHome = returnHome;
     }
 
-    PageTestController.$inject = ['$scope', '$timeout', 'bbPage'];
+    PageTestController.$inject = ['$scope', '$timeout', 'bbPage', '$location'];
 
-    angular.module('stache').controller('PageTestController', PageTestController);
+    function bbPageSetup(bbPageConfig, $stateProvider, $urlRouterProvider) {
+        bbPageConfig.redirectUrl = '/components';
+        bbPageConfig.notFoundUrl = '/notfound';
+        
+         $stateProvider
+            .state('default', {
+                url: '/',
+                templateUrl: 'demo/page/buttons.html'
+            })
+            .state('notFoundState', {
+                url: '/notfound',
+                templateUrl: 'demo/page/notfound.html'
+            });
+         $urlRouterProvider.otherwise('/');
+    }
+    
+    bbPageSetup.$inject = ['bbPageConfig', '$stateProvider', '$urlRouterProvider'];
+
+    angular.module('stache')
+        .controller('PageTestController', PageTestController)
+        .config(bbPageSetup);
 }());
--- js/sky/src/page/docs/demo.md
+++ js/sky/src/page/docs/demo.md
@@ -7,9 +7,13 @@ summary: The page component handles functionality around loading pages.
 The page directive handles functionality around loading pages.
 
 ### Page settings ###
-- `bb-page-status` &mdash; Indicates the status of the page.
+- `bb-page`
+  - `bb-page-status` &mdash; Indicates the status of the page.
     - `LOADING` &mdash; Denotes that the page is loading.
     - `LOADED` &mdash; Denotes that the page loaded successfully.
     - `NOT_AUTHORIZED` &mdash; Denotes that the page loaded and displays a message that the user does not have right to view the content on the page.
     - `NOT_FOUND` &mdash; Denotes that the page was not found and should redirect to the not-found page.
-- `bb-page-uses-load-manager` &mdash; Allows the page to use the `bb-data` load manager.
+  - `bb-page-uses-load-manager` &mdash; Allows the page to use the `bb-data` load manager to detect when components within `bb-page` are loading.
+- `bb-page-config`
+  - `redirectUrl` &mdash; *(Optional.)* Provides a url for the `NOT_AUTHORIZED` redirect button. If not specified, the `NOT_AUTHORIZED` redirect button sends the user to the website root.
+  - `notFoundUrl` &mdash; *(Optional.)* Provides a url that `$location.path` will be set to when `bb-page-status` is set to `NOT_FOUND`.
