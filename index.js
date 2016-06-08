87b119fcd543aa772d21064d4c73aa19831f11ac
Blackbaud-JasonBilyeu <jason.bilyeu@blackbaud.com>
2016-06-08 12:13:33 -0500

    Add an index.js and reference it as the main from package.json so we can pull sky in through commonJS.

---
+++ index.js
@@ -0,0 +1,21 @@
+window.jQuery = require('jquery');
+require("jquery-ui-bundle");
+require("jquery-ui-touch-punch");
+require("angular");
+require("angular-animate");
+require("angular-messages");
+require("angular-toastr");
+require("angular-ui-bootstrap");
+require("angular-ui-router");
+require("autonumeric");
+require("block-ui");
+require("bootstrap");
+require("enquire.js");
+require("fastclick");
+require("flot-charts");
+require("free-jqgrid/js/jquery.jqGrid");
+require("moment");
+require("ng-file-upload");
+require("./dist/js/sky");
+
+module.exports = 'sky';
