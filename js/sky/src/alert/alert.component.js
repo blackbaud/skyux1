478b5c72f22fe38304d4fdc50f88963ca21391de
Blackbaud-CeciliaPrentice <Cecilia.Prentice@Blackbaud.com>
2016-06-10 10:15:09 -0400

    deleted all keyinfo related items

--- js/sky/src/alert/alert.component.js
+++ js/sky/src/alert/alert.component.js
@@ -23,21 +23,3 @@
             transclude: true
         });
 }());
-
-/*global angular */
-
-(function () {
-    'use strict';
- 
-    angular.module('sky.keyinfo.component', [])
-        .component('bbKeyInfo', {
-            bindings: {
-                bbKeyInfoLayout: '@?'
-            }, 
-            templateUrl: 'sky/templates/keyinfo/keyinfo.component.html',
-            transclude: {
-                value: 'bbKeyInfoValue',
-                label: 'bbKeyInfoLabel'
-            }
-        });
-}());
