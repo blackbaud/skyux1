6c4d19158d3ec68c7772d3f98ed946fde49696d1
Blackbaud-CeciliaPrentice <Cecilia.Prentice@Blackbaud.com>
2016-06-14 16:12:39 -0400

    changed component file

--- js/sky/src/navbar/navbar.component.js
+++ js/sky/src/navbar/navbar.component.js
@@ -6,27 +6,28 @@
     function toggleOpen(el, action) {
         $(el)[action + 'Class']('open');
     }
-
-    function Controller($element) {
-        
-        /*jslint unparam: true */
-        ($element).on('mouseenter', '.dropdown', function () {
-            toggleOpen(this, 'add');
-        }).on('mouseleave', '.dropdown', function () {
-            toggleOpen(this, 'remove');
-        }).on('click', '.dropdown-menu a', function () {
-            toggleOpen($('.dropdown', $element), 'remove');
-        });
-    } 
-    
-    Controller.$inject = ['$element'];
     
     angular.module('sky.navbar', [])
-        .component('bbNavbar', {
+        .component('bbNavbar', function () {
+            function Controller($element) {
+        
+                /*jslint unparam: true */
+                ($element).on('mouseenter', '.dropdown', function () {
+                    toggleOpen(this, 'add');
+                }).on('mouseleave', '.dropdown', function () {
+                    toggleOpen(this, 'remove');
+                }).on('click', '.dropdown-menu a', function () {
+                    toggleOpen($('.dropdown', $element), 'remove');
+                });
+            } 
+    
+            Controller.$inject = ['$element']; 
+            {
             replace: true,
             transclude: true,
             restrict: 'E',
             templateUrl: 'sky/templates/navbar/navbar.component.html', 
-            controller: Controller
+            controller: Controller 
+        }
         });
 }(jQuery));
