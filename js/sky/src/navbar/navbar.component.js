76423540beb37ebf790600e708cc06d58b543470
Blackbaud-CeciliaPrentice <Cecilia.Prentice@Blackbaud.com>
2016-06-14 15:13:17 -0400

    fixed element in component function

--- js/sky/src/navbar/navbar.component.js
+++ js/sky/src/navbar/navbar.component.js
@@ -7,23 +7,26 @@
         $(el)[action + 'Class']('open');
     }
 
-    function Controller() {
-        var el = this;
+    function Controller($element) {
+        
         /*jslint unparam: true */
-        $(el).on('mouseenter', '.dropdown', function () {
+        $element.on('mouseenter', '.dropdown', function () {
             toggleOpen(this, 'add');
         }).on('mouseleave', '.dropdown', function () {
             toggleOpen(this, 'remove');
         }).on('click', '.dropdown-menu a', function () {
-            toggleOpen($('.dropdown', el), 'remove');
+            toggleOpen($('.dropdown', $element), 'remove');
         });
     } 
+    
+    Controller.$inject = ['$element'];
+    
     angular.module('sky.navbar', [])
         .component('bbNavbar', {
             replace: true,
             transclude: true,
             restrict: 'E',
-            templateUrl: 'sky/templates/navbar/navbar.component.html',
+            templateUrl: 'sky/templates/navbar/navbar.component.html', 
             controller: Controller
         });
 }(jQuery));
