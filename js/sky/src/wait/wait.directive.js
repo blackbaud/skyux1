7504da451edf17d90bb33520c44323b60af6eb6d
Blackbaud-CeciliaPrentice <Cecilia.Prentice@Blackbaud.com>
2016-06-22 11:30:11 -0400

    initial changes

---
+++ js/sky/src/wait/wait.directive.js
@@ -0,0 +1,25 @@
+/*jslint browser: true */
+/*global angular, jQuery */
+
+(function ($) {
+    'use strict';
+
+        .directive('bbWait', ['bbWait', function (bbWait) {
+            /// <summary>
+            /// This directive provides an attribute that can be placed on elements indicating whether they should or shouldn't be blocked for waiting.
+            /// </summary>
+            return {
+                restrict: 'A',
+                link: function (scope, el, attrs) {
+                    var firstScopeLoad = true;
+                    scope.$watch(attrs.bbWait, function (value, oldValue) {
+                        if (value && (!oldValue || firstScopeLoad)) {
+                            bbWait.beginElWait(el);
+                        } else if (oldValue && !value) {
+                            bbWait.endElWait(el);
+                        }
+                        firstScopeLoad = false;
+                    });
+                }
+            };
+        }]);
--- js/sky/src/wait/wait.factory.js
+++ js/sky/src/wait/wait.factory.js
@@ -4,7 +4,6 @@
 (function ($) {
     'use strict';
 
-    angular.module('sky.wait', [])
         .factory('bbWait', ['$timeout', function ($timeout) {
 
             var addWait,
@@ -252,25 +251,7 @@
                     removeWait(document.body, options);
                 }
             };
-        }])
-        .directive('bbWait', ['bbWait', function (bbWait) {
-            /// <summary>
-            /// This directive provides an attribute that can be placed on elements indicating whether they should or shouldn't be blocked for waiting.
-            /// </summary>
-            return {
-                restrict: 'A',
-                link: function (scope, el, attrs) {
-                    var firstScopeLoad = true;
-                    scope.$watch(attrs.bbWait, function (value, oldValue) {
-                        if (value && (!oldValue || firstScopeLoad)) {
-                            bbWait.beginElWait(el);
-                        } else if (oldValue && !value) {
-                            bbWait.endElWait(el);
-                        }
-                        firstScopeLoad = false;
-                    });
-                }
-            };
         }]);
 
 }(jQuery));
+
