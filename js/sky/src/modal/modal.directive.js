88ab06dafe212c519668914c73ff7d408285c32d
Blackbaud-CeciliaPrentice <Cecilia.Prentice@Blackbaud.com>
2016-06-23 09:55:41 -0400

    initial changes

---
+++ js/sky/src/modal/modal.directive.js
@@ -0,0 +1,159 @@
+/*jshint browser: true */
+/*global angular, jQuery */
+
+(function ($) {
+    'use strict';
+
+    angular.module('sky.modal', ['sky.helpbutton', 'sky.resources', 'ui.bootstrap'])
+        .directive('bbModal', ['$timeout', function ($timeout) {
+            function getPixelValue(val) {
+                val = val || '0';
+
+                return parseFloat(val.replace('px', ''));
+            }
+
+            function getModalBodyWrapperMargin(el) {
+                var margin = 0;
+
+                while (el.not('.modal-dialog') && el.length > 0) {
+                    margin += el.outerHeight() - el.height();
+
+                    el = el.parent();
+                }
+
+                return margin;
+            }
+
+            return {
+                controller: ['$scope', function ($scope) {
+                    this.setBodyEl = function (bodyEl) {
+                        $scope.bodyEl = bodyEl;
+                    };
+                }],
+                replace: true,
+                transclude: true,
+                restrict: 'E',
+                templateUrl: 'sky/templates/modal/modal.html',
+                link: function ($scope, el) {
+                    var bodyEl,
+                        resizeTimeout,
+                        windowEl = $(window);
+
+                    function fitToWindow() {
+                        var margin,
+                            modalParentEl,
+                            newMaxHeight,
+                            reservedHeight;
+
+                        if (bodyEl && bodyEl.length > 0) {
+                            modalParentEl = el.parents('.modal-dialog');
+
+                            if (modalParentEl.length > 0) {
+                                margin = getPixelValue(modalParentEl.css('margin-bottom')) + getPixelValue(modalParentEl.css('margin-top'));
+
+                                reservedHeight = margin + el.find('.modal-header').outerHeight() + el.find('.modal-footer').outerHeight();
+
+                                // Account for the border, padding, etc. of the elements that wrap the modal body.
+                                reservedHeight += getModalBodyWrapperMargin(el);
+
+                                newMaxHeight = windowEl.height() - reservedHeight;
+
+                                bodyEl.css('max-height', newMaxHeight);
+                            }
+                        }
+                    }
+
+                    $scope.$watch('bodyEl', function (newValue) {
+                        bodyEl = newValue;
+                        fitToWindow();
+                    });
+
+                    $timeout(function () {
+                        fitToWindow();
+                    }, 0);
+
+                    windowEl.on('resize.bbModal' + $scope.$id, function () {
+                        $timeout.cancel(resizeTimeout);
+
+                        resizeTimeout = $timeout(function () {
+                            fitToWindow();
+                        }, 250);
+                    });
+
+                    el.on('$destroy', function () {
+                        windowEl.off('.bbModal' + $scope.$id);
+                    });
+                }
+            };
+        }])
+        .directive('bbModalBody', function () {
+            return {
+                link: function (scope, el, attrs, modalCtrl) {
+                    el.addClass('modal-body container-fluid');
+                    modalCtrl.setBodyEl(el);
+                },
+                require: '^bbModal',
+                restrict: 'A'
+            };
+        })
+        .directive('bbModalHeader', function () {
+            return {
+                controller: angular.noop,
+                replace: true,
+                transclude: true,
+                require: '^bbModal',
+                restrict: 'E',
+                templateUrl: 'sky/templates/modal/modalheader.html',
+                scope: {
+                    bbModalHelpKey: '='
+                }
+            };
+        })
+        .directive('bbModalFooter', function () {
+            return {
+                controller: angular.noop,
+                replace: true,
+                transclude: true,
+                require: '^bbModal',
+                restrict: 'E',
+                templateUrl: 'sky/templates/modal/modalfooter.html'
+            };
+        })
+        .directive('bbModalFooterButton', function () {
+            return {
+                replace: true,
+                transclude: true,
+                require: '^bbModalFooter',
+                restrict: 'E',
+                templateUrl: 'sky/templates/modal/modalfooterbutton.html'
+            };
+        })
+        .directive('bbModalFooterButtonPrimary', ['bbResources', function (bbResources) {
+            return {
+                replace: true,
+                transclude: true,
+                require: '^bbModalFooter',
+                restrict: 'E',
+                templateUrl: 'sky/templates/modal/modalfooterbuttonprimary.html',
+                link: function ($scope, el) {
+                    if (el.children().length === 0) {
+                        el.append("<span>" + bbResources.modal_footer_primary_button + "</span>");
+                    }
+                }
+            };
+        }])
+        .directive('bbModalFooterButtonCancel', ['bbResources', function (bbResources) {
+            return {
+                replace: true,
+                transclude: true,
+                require: '^bbModalFooter',
+                restrict: 'E',
+                templateUrl: 'sky/templates/modal/modalfooterbuttoncancel.html',
+                link: function ($scope, el) {
+                    if (el.children().length === 0) {
+                        el.append("<span>" + bbResources.modal_footer_cancel_button + "</span>");
+                    }
+                }
+            };
+        }]);
+}(jQuery));
---
+++ js/sky/src/modal/modal.module.js
@@ -0,0 +1,8 @@
+/*global angular */
+
+(function () {
+    'use strict';
+
+    angular.module('sky.modal', ['sky.helpbutton', 'sky.resources', 'ui.bootstrap'])
+
+}());
