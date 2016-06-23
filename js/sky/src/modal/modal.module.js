1368896920f017120650c721dba7712b658b5396
Blackbaud-CeciliaPrentice <Cecilia.Prentice@Blackbaud.com>
2016-06-23 11:01:26 -0400

    put directives into separate files

--- js/sky/src/modal/modal.module.js
+++ js/sky/src/modal/modal.module.js
@@ -3,6 +3,14 @@
 (function () {
     'use strict';
 
-    angular.module('sky.modal', ['sky.modal.directive', 'sky.modal.factory']);
+    angular.module('sky.modal', 
+    ['sky.modal.directive', 
+    'sky.modalbody.directive', 
+    'sky.modalfooter.directive', 
+    'sky.modalfooterbutton.directive', 
+    'sky.modalfooterbuttoncancel.directive',
+    'sky.modalfooterbuttonprimary.directive', 
+    'sky.modalheader.directive',  
+    'sky.modal.factory']);
 
 }());
---
+++ js/sky/src/modal/modalfooter.directive.js
@@ -0,0 +1,18 @@
+/*jshint browser: true */
+/*global angular, jQuery */
+
+(function () {
+    'use strict';
+
+    angular.module('sky.modalfooter.directive', ['sky.helpbutton', 'sky.resources', 'ui.bootstrap'])
+        .directive('bbModalFooter', function () {
+            return {
+                controller: angular.noop,
+                replace: true,
+                transclude: true,
+                require: '^bbModal',
+                restrict: 'E',
+                templateUrl: 'sky/templates/modal/modalfooter.html'
+            };
+        });
+}(jQuery));
