8dd100f79ae357d8a6b8ea228c8d3ed48878bbc9
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-06-23 16:12:11 -0400

    move listbuilder controller into component

--- js/sky/src/listbuilder/listbuilder.component.js
+++ js/sky/src/listbuilder/listbuilder.component.js
@@ -2,16 +2,41 @@
 (function () {
     'use strict';
 
-    var listbuilderComponent = {
-        templateUrl: 'sky/templates/listbuilder/listbuilder.component.html',
-        transclude: {
-            bbListbuilderToolbar: '?bbListbuilderToolbar',
-            bbListbuilderContent: '?bbListbuilderContent',
-            bbListbuilderFooter: '?bbListbuilderFooter'
-        },
-        controller: 'BBListbuilderController'
-    };
+    function Controller($element, bbHighlight, $timeout) {
+        var ctrl = this;
 
-    angular.module('sky.listbuilder.component', ['sky.listbuilder.controller', 'sky.card'])
-        .component('bbListbuilder', listbuilderComponent);
+        function highlightSearchText(searchText) {
+            $timeout(function () {
+                var cardEl = $element.find('.bb-card');
+                if (cardEl.length > 0) {
+                    bbHighlight.clear(cardEl);
+                    if (searchText) {
+                        bbHighlight(cardEl.not('.bb-listbuilder-no-search'), searchText, 'highlight');
+                    }
+                }
+                
+            });
+           
+        }
+
+        function getContentContainer() {
+            return $element.find('.bb-listbuilder-content');
+        }     
+
+        ctrl.highlightSearchText = highlightSearchText;
+        ctrl.getContentContainer = getContentContainer;
+    }
+
+    Controller.$inject = ['$element', 'bbHighlight', '$timeout'];
+
+    angular.module('sky.listbuilder.component', ['sky.card', 'sky.highlight'])
+        .component('bbListbuilder', {
+            templateUrl: 'sky/templates/listbuilder/listbuilder.component.html',
+            transclude: {
+                bbListbuilderToolbar: '?bbListbuilderToolbar',
+                bbListbuilderContent: '?bbListbuilderContent',
+                bbListbuilderFooter: '?bbListbuilderFooter'
+            },
+            controller: Controller
+        });
 }());
