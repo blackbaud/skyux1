569d013f1b0d02e84bb1d1b4aa3e2d4258d1386e
Blackbaud-CeciliaPrentice <Cecilia.Prentice@Blackbaud.com>
2016-06-27 14:47:13 -0400

    created files and made initial changes

---
+++ js/sky/src/microlayout/docs/demo.js
@@ -0,0 +1,12 @@
+/*global angular */
+
+(function () {
+    'use strict';
+
+    function MicrolayoutTestController() {
+
+    }
+
+    angular.module('stache')
+        .controller('MicrolayoutTestController', MicrolayoutTestController);
+}());
---
+++ js/sky/src/microlayout/microlayout.component.js
@@ -0,0 +1,16 @@
+/*global angular */
+
+(function () {
+    'use strict';
+    
+    angular.module('sky.microlayout.component', [])
+        .component('bbMicrolayout', {
+            bindings: {
+                
+            }, 
+            templateUrl: 'sky/templates/microlayout/microlayout.component.html',
+            transclude: {
+                
+            }
+        });
+}());
