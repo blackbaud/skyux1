312e87e16130ad9140a24d3374a521901ec0f93a
Paul Crowder <paul.crowder@blackbaud.com>
2016-07-14 11:34:08 -0400

    View keeper refactor in preparation for scrollable parents

--- js/sky/src/modal/docs/demo.js
+++ js/sky/src/modal/docs/demo.js
@@ -16,6 +16,72 @@
         var vm = this;
 
         vm.textLabel = 'Sample text box';
+
+        vm.gridOptions = {
+            columns: [
+                {
+                    caption: 'Name',
+                    jsonmap: 'name',
+                    id: 1,
+                    name: 'name',
+                    width_xs: 100,
+                    width_all: 300
+                },
+                {
+                    caption: 'Skills',
+                    jsonmap: 'skills',
+                    id: 2,
+                    name: 'skills',
+                    allow_see_more: true,
+                    width_all: 100
+                },
+                {
+                    caption: 'Number of cats',
+                    jsonmap: 'cats',
+                    id: 3,
+                    name: 'cats'
+                }
+            ],
+            data: [
+                {
+                    name: 'Patrick',
+                    skills: 'Karate, Kung Fu, Italian cooking, Ditch digging'
+                },
+                {
+                    name: 'Paul',
+                    skills: 'Arguing',
+                    cats: '13'
+                },
+                {
+                    name: 'George',
+                    skills: 'Curiousity',
+                    cats: '1'
+                },
+                {
+                    name: 'Ringo',
+                    skills: 'Slow typing'
+                },
+                {
+                    name: 'Samwise',
+                    skills: 'Loyalty, Gardening'
+                }
+            ],
+            onAddClick: function () {
+                alert('Add button clicked!!');
+            },
+            onAddClickLabel: 'Add button',
+            selectedColumnIds: [1, 2, 3],
+            columnPickerHelpKey: 'bb-security-users.html',
+            sortOptions: {
+                excludedColumns: [
+                    'cats',
+                    'name',
+                    'skills'
+                ]
+            },
+            hasInlineFilters: true,
+            filters: {}
+        };
     }
 
     ModalTestController.$inject = ['bbModal'];
