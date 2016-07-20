0cd63994efb9ce0abb91e399cae2c69288d376a4
Blackbaud-CeciliaPrentice <Cecilia.Prentice@Blackbaud.com>
2016-07-20 08:37:24 -0400

    added divs

--- js/sky/src/popover/docs/demo.js
+++ js/sky/src/popover/docs/demo.js
@@ -7,10 +7,10 @@
         // purposes we'll just manually put the template in the cache.
         $templateCache.put('bbPopoverTemplate/samples/samplepopover.html',
             '<div class="tooltip-container">' +
-                '<label>' +
+                '<div><label>' +
                 '<div>Select a beverage:</div>' +
                 '<div><select ng-model="popCtrl.selectedItem" ng-options="item as item for item in popCtrl.items"></select></div>' +
-                '</label>' +
+                '</label></div>' +
                 '<a ng-click="hide()">Close me</a>' +
             '</div>');
     }
