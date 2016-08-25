338e84ff45ddf64e8a44d28ba984368f519841b2
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-08-25 17:38:53 -0400

    mess with full page modal visual tests

--- webdrivertest/test/modal/modal.visual.js
+++ webdrivertest/test/modal/modal.visual.js
@@ -37,7 +37,6 @@ describe('modals', function () {
             .pause(1000)
             .compareScreenshot({
                 screenshotName: 'modal_fullpage',
-                selector: 'html',
                 checkAccessibility: true
             })
             .click('.bb-modal .modal-dialog .close')
