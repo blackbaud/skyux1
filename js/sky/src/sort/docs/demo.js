85c113f20726b8b28a89bd949f6a1ad8cfe7f2e1
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-09-06 15:34:25 -0400

    start of sort component
    mockup in the demo

---
+++ js/sky/src/sort/docs/demo.js
@@ -0,0 +1,63 @@
+(function () {
+    'use strict';
+
+
+    function SortTestController() {
+        var self = this;
+
+        function selectItem(item) {
+            self.selectedItem = item;
+        }
+
+
+        self.selectItem = selectItem;
+
+        self.items = [
+            {
+                id: 1,
+                label: 'Last invoice amount (largest first)',
+                name: 'lastInvoiceLarge'
+            },
+            {
+                id: 2,
+                label: 'Last invoice amount (smallest first)',
+                name: 'lastInvoiceSmall'
+            },
+            {
+                id: 3,
+                label: 'Last invoice date (newest first)',
+                name: 'lastInvoiceNew'
+            },
+            {
+                id: 4,
+                label: 'Last invoice date (oldest first)',
+                name: 'lastInvoiceOld'
+            },
+            {
+                id: 5,
+                label: 'Total balance due (largest first)',
+                name: 'totalBalanceLarge'
+            },
+            {
+                id: 6,
+                label: 'Total balance due (smallest first)',
+                name: 'totalBalanceSmall'
+            },
+            {
+                id: 7,
+                label: 'Vendor name (A - Z)',
+                name: 'vendorNameAZ'
+            },
+            {
+                id: 8,
+                label: 'Vendor name (Z - A)',
+                name: 'vendorNameZA'
+            }
+        ];
+    }
+
+    SortTestController.$inject = ['bbModal'];
+
+    angular.module('stache')
+        .controller('SortTestController', SortTestController);
+})();
---
+++ js/sky/src/sort/docs/demo.md
@@ -0,0 +1,10 @@
+---
+name: Sort
+icon: sort
+summary: The sort component certainly does something.
+---
+
+The sort component certainly does something.
+
+### Sort settings ###
+  - `bb-sort` &mdash; Creates a sort.
