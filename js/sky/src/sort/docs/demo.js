1acf794caba8103c04abb7945bd24d84b64ed9db
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-09-08 14:14:30 -0400

    Update demo with sortable repeater

--- js/sky/src/sort/docs/demo.js
+++ js/sky/src/sort/docs/demo.js
@@ -3,59 +3,106 @@
 
 
     function SortTestController() {
-        var self = this;
+        var self = this,
+            repeaterItems;
 
         function selectItem(item) {
-            self.selectedItem = item;
+            var result = repeaterItems.sort(function (a, b) {
+                switch (item.name) {
+                    case 'assigneeNameAZ': 
+                        return a.assignee > b.assignee;
+                    case 'assigneeNameZA': 
+                        return a.assignee < b.assignee;
+                    case 'dateCreatedNew':
+                        return a.date < b.date;
+                    case 'dateCreatedOld': 
+                        return a.date > b.date;
+                    case 'noteTitleAZ':
+                        return a.title > b.title;
+                    case 'notTitleZA':
+                        return a.title < b.title;   
+                }
+                return true;
+            });
+
+            self.sortedItems = repeaterItems;
         }
 
-
+        function initSort() {
+            self.initialState = self.sortOptions[4].id;
+            selectItem(self.sortOptions[4]);
+        }
+        
+        repeaterItems = [
+            {
+                title: 'Call Robert Hernandez',
+                note: 'Robert recently gave a very generous gift. We should call to thank him.',
+                assignee: 'Debby Fowler',
+                date: new Date('12/22/2015')
+            },
+            {
+                title: 'Send invitation to ball',
+                note: 'The Spring Ball is coming up soon. Let\'s get those invitations out!',
+                assignee: 'Debby Fowler',
+                date: new Date('1/1/2016')
+            },
+            {
+                title: 'Clean up desk',
+                note: 'File and organize papers.',
+                assignee: 'Tim Howard',
+                date: new Date('2/2/2016')
+            },
+            {
+                title: 'Investigate leads',
+                note: 'Check out leads for important charity event funding.',
+                assignee: 'Larry Williams',
+                date: new Date('4/5/2016')
+            },
+            {
+                title: 'Send thank you note',
+                note: 'Send a thank you note to Timothy for his donation.',
+                assignee: 'Catherine Hooper',
+                date: new Date('11/11/2015')
+            }
+        ];
+        
         self.sortItems = selectItem;
 
-        self.items = [
+        self.sortOptions = [
             {
                 id: 1,
-                label: 'Last invoice amount (largest first)',
-                name: 'lastInvoiceLarge'
+                label: 'Assignee name (A - Z)',
+                name: 'assigneeNameAZ'
             },
             {
                 id: 2,
-                label: 'Last invoice amount (smallest first)',
-                name: 'lastInvoiceSmall'
+                label: 'Assignee name (Z - A)',
+                name: 'assigneeNameZA'
             },
             {
                 id: 3,
-                label: 'Last invoice date (newest first)',
-                name: 'lastInvoiceNew'
+                label: 'Date created (newest first)',
+                name: 'dateCreatedNew'
             },
             {
                 id: 4,
-                label: 'Last invoice date (oldest first)',
-                name: 'lastInvoiceOld'
+                label: 'Date created (oldest first)',
+                name: 'dateCreatedOld'
             },
             {
                 id: 5,
-                label: 'Total balance due (largest first)',
-                name: 'totalBalanceLarge'
+                label: 'Note title (A - Z)',
+                name: 'noteTitleAZ'
             },
             {
                 id: 6,
-                label: 'Total balance due (smallest first)',
-                name: 'totalBalanceSmall'
-            },
-            {
-                id: 7,
-                label: 'Vendor name (A - Z)',
-                name: 'vendorNameAZ'
-            },
-            {
-                id: 8,
-                label: 'Vendor name (Z - A)',
-                name: 'vendorNameZA'
+                label: 'Note title (Z - A)',
+                name: 'noteTitleZA'
             }
         ];
 
-        self.initialState = self.items[6].id;
+        initSort();
+        
     }
 
     SortTestController.$inject = ['bbModal'];
