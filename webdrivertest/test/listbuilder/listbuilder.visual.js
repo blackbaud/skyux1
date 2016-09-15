1e0e9a0b0e96b7b4b3a69811ef8c27a9d24d8768
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-09-15 14:42:01 -0400

    Update listbuilder visual tests

--- webdrivertest/test/listbuilder/listbuilder.visual.js
+++ webdrivertest/test/listbuilder/listbuilder.visual.js
@@ -4,24 +4,22 @@ describe('listbuilder', function () {
     'use strict';
 
 
-    function listbuilderTest(screenWidth, done) {
-        var common = require('../common');
+    function listbuilderTest(screenWidth) {
 
-        browser
+        return browser
             .setupTest('/listbuilder/fixtures/test.full.html', screenWidth)
             .compareScreenshot({
-                prefix: common.getPrefix(browser),
                 screenshotName: 'listbuilder',
-                selector: '#screenshot-listbuilder'
-            })
-            .call(done);
+                selector: '#screenshot-listbuilder',
+                checkAccessibility: true
+            });
     }
 
-    it('match the baseline listbuilder screenshot', function (done) {
-        listbuilderTest(1280, done);
+    it('match the baseline listbuilder screenshot', function () {
+        listbuilderTest(1280);
     });
 
-    it('match the baseline listbuilder screenshot on small screens', function (done) {
-        listbuilderTest(480, done);
+    it('match the baseline listbuilder screenshot on small screens', function () {
+        listbuilderTest(480);
     });
 });
