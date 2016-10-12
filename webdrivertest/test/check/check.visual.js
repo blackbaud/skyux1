34ed4b1bb74e3f73cbd33dee4e01954fea9b5104
Paul Crowder <paul.crowder@blackbaud.com>
2016-10-12 11:38:32 -0400

    Added visual regression tests

--- webdrivertest/test/check/check.visual.js
+++ webdrivertest/test/check/check.visual.js
@@ -12,4 +12,31 @@ describe('check', function () {
                 checkAccessibility: true
             });
     });
+
+    it(
+        'should match the baseline check screenshot when a checkbox is in an inline form', 
+        function () {
+            return browser
+                .setupTest('/check/fixtures/test.full.html')
+                .compareScreenshot({
+                    screenshotName: 'check_inline',
+                    selector: '#screenshot-check-inline',
+                    checkAccessibility: true
+                });
+        }
+    );
+
+    it(
+        'should match the baseline check screenshot when a checkbox is in an inline form ' +
+        'at a small screen size', 
+        function () {
+            return browser
+                .setupTest('/check/fixtures/test.full.html', 480)
+                .compareScreenshot({
+                    screenshotName: 'check_inline_small',
+                    selector: '#screenshot-check-inline',
+                    checkAccessibility: true
+                });
+        }
+    );
 });
