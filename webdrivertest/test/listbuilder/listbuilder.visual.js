9cd70d7115281f5e419fb3694e56ac4fd1954d47
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-10-18 15:45:00 -0400

    update listbuilder visual tests

--- webdrivertest/test/listbuilder/listbuilder.visual.js
+++ webdrivertest/test/listbuilder/listbuilder.visual.js
@@ -22,4 +22,29 @@ describe('listbuilder', function () {
     it('match the baseline listbuilder screenshot on small screens', function () {
         return listbuilderTest(480);
     });
+
+    it('match the listbuilder open view switcher screenshot', function () {
+        return browser
+            .setupTest('/listbuilder/fixtures/test.full.html')
+            .click('.bb-listbuilder-switcher button')
+            .pause(1000)
+            .compareScreenshot({
+                screenshotName: 'listbuilder_switcher',
+                selector: '#screenshot-listbuilder',
+                checkAccessibility: true
+            });
+    });
+
+    it('match the listbuilder repeater screenshot', function () {
+        return browser
+            .setupTest('/listbuilder/fixtures/test.full.html')
+            .click('.bb-listbuilder-switcher button')
+            .click('.bb-listbuilder-switcher-menu a')
+            .pause(1000)
+            .compareScreenshot({
+                screenshotName: 'listbuilder_repeater',
+                selector: '#screenshot-listbuilder',
+                checkAccessibility: true
+            });
+    });
 });
