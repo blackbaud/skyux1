853ffcb425a59be2b75d0de479fd8e99e840fb69
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-10-26 14:15:29 -0400

    continued work on summary-actionbar
    - Tweak styles
    - Make collapse work correctly
    - add margin to body to account for summary bar height
    
    TODO:
      - collapse/expand animation
      - secondary actions
      - make bar work correctly in modal
      - make bar work correctly in full page modal

--- js/sky/src/summaryactionbar/summary.actionbar.component.js
+++ js/sky/src/summaryactionbar/summary.actionbar.component.js
@@ -2,17 +2,70 @@
 (function () {
     'use strict';
 
-    function Controller($document, $element) {
-        var ctrl = this;
+    function Controller($document, $element, $transclude, $scope, $timeout, $window, bbMediaBreakpoints) {
+        var ctrl = this,
+            marginTimeout,
+            actionbarEl;
+
+        function toggleDetails() {
+            ctrl.hideSummary = !ctrl.hideSummary;
+            addActionbarMargin();
+        }
+
+        function breakpointChanged(breakpoint) {
+            ctrl.isXsScreen = breakpoint && breakpoint.xs;
+        }
+
+        function addActionbarMargin() {
+            $timeout.cancel(marginTimeout);
+            marginTimeout = $timeout(function () {
+                var actionbarHeight = actionbarEl.outerHeight();
+                $document.find('body').css('margin-bottom', actionbarHeight);
+            }, 250);
+        }
+
+        function getActionbar() {
+            return $element.find('.bb-summary-actionbar');
+        }
+
+        function watchActionBarHeight() {
+            $scope.$watch(function () {
+                return actionbarEl.outerHeight();
+            }, function (newValue, oldValue) {
+                if (oldValue !== newValue) {
+                    addActionbarMargin();
+                }
+            });
+        }
+
+        function windowResize() {
+            angular.element($window).on('resize.bbSummaryActionbar' + $scope.$id, function () {
+                addActionbarMargin();
+            });
+        }
 
         function onInit() {
-            $document.find('body').append($element.find('.bb-summary-actionbar'));
+            actionbarEl = getActionbar();
+            bbMediaBreakpoints.register(breakpointChanged);
+            ctrl.summaryContentExists = $transclude.isSlotFilled('bbSummaryActionbarSummary');
+            watchActionBarHeight();
+            windowResize();
+        }
+
+        function onDestroy() {
+            bbMediaBreakpoints.unregister(breakpointChanged);
+            $timeout.cancel(marginTimeout);
+            $document.find('body').css('margin-bottom', '');
+            angular.element($window).off('resize.bbSummaryActionbar' + $scope.$id);
         }
 
         ctrl.$postLink = onInit;
+        ctrl.$onDestroy = onDestroy;
+
+        ctrl.toggleDetails = toggleDetails;
     }
 
-    Controller.$inject = ['$document', '$element'];
+    Controller.$inject = ['$document', '$element', '$transclude', '$scope', '$timeout', '$window', 'bbMediaBreakpoints'];
 
     angular.module('sky.summary.actionbar.component', [])
         .component('bbSummaryActionbar', {
