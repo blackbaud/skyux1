627651701a4aa634675b54b32d0fa20c15373d6a
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-10-28 17:45:36 -0400

    Summary actionbar unit test stubs

---
+++ js/sky/src/summaryactionbar/test/summary.actionbar.component.spec.js
@@ -0,0 +1,58 @@
+/* jshint jasmine: true */
+(function () {
+    'use strict';
+
+    describe('Summary actionbar component', function () {
+        it('should create an actionbar with action and summary sections', function () {
+
+        });
+
+        it('should have actions collapse into a dropdown when more than 2 actions exist', function () {
+
+        });
+
+        it('should have actions collapse into a dropdown when on a small screen', function () {
+
+        });
+
+        it('should show the expanded summary when on a small screen and allow toggle of hide and show', function () {
+
+        });
+
+        it('should adjust the margin on the document when window resizes', function () {
+
+        });
+
+        it('should adjust the margin on the document as the summary height changes', function () {
+
+        });
+
+        describe('modal', function () {
+            it('should set modal styles and call fit to window when in a modal', function () {
+
+            });
+
+            describe('no full page', function () {
+                it('should animate the modal body and fit to window when expanding and collapsing the summary', function () {
+
+                }); 
+
+                it('should be always in collapse mode when in a modal', function () {
+
+                });
+            });
+
+            describe('full page', function () {
+                it('should set modal body styles on hiding modal', function () {
+
+                });
+
+                it('should show the expanded summary when on a small screen and side by side on a large screen', function () {
+
+                });
+            });
+
+            
+        });
+    });
+})();
