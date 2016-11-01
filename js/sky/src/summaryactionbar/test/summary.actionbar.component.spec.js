848cf012a73237135f081c26c39c713667dc24ff
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-11-01 09:41:58 -0400

    start of unit tests

--- js/sky/src/summaryactionbar/test/summary.actionbar.component.spec.js
+++ js/sky/src/summaryactionbar/test/summary.actionbar.component.spec.js
@@ -1,9 +1,56 @@
 /* jshint jasmine: true */
+/* global module, angular, inject, $ */
 (function () {
     'use strict';
+    
 
     describe('Summary actionbar component', function () {
+        var $compile,
+            $scope,
+            $document,
+            bbMediaBreakpoints,
+            fxOff;
+
+        beforeEach(module(
+            'sky.summary.actionbar',
+            'sky.templates'
+        ));
+
+        beforeEach(inject(function (_$rootScope_, _$compile_, _$document_, _bbMediaBreakpoints_) {
+            $scope = _$rootScope_.$new();
+            $compile = _$compile_;
+            $document = _$document_;
+            bbMediaBreakpoints = _bbMediaBreakpoints_;
+            fxOff = $.fx.off;
+            $.fx.off = true;
+        }));
+
+        afterEach(function () {
+            $.fx.off = fxOff;
+        });
+
         it('should create an actionbar with action and summary sections', function () {
+            var actionbarHtml = '<bb-summary-actionbar>' +
+                '<bb-summary-actionbar-actions>' +
+                '<bb-summary-actionbar-primary>Primary action</bb-summary-actionbar-primary>' +
+                '<bb-summary-actionbar-secondary-actions>' +
+                '<bb-summary-actionbar-secondary ng-click="summaryCtrl.action1()">Secondary action</bb-summary-actionbar-secondary>' +
+                '<bb-summary-actionbar-secondary ng-click="summaryCtrl.action2()">Secondary action 2</bb-summary-actionbar-secondary>' +
+                '</bb-summary-actionbar-secondary-actions>' +
+                '<bb-summary-actionbar-cancel>Cancel</bb-summary-actionbar-cancel>' +
+                '</bb-summary-actionbar-actions>' +
+                '<bb-summary-actionbar-summary>' +
+                '<div class="bb-test-summary">' +
+                'My Content' +
+                '</div>' +
+                '</bb-summary-actionbar-summary>' +
+                '</bb-summary-actionbar>',
+                actionbarEl;
+
+            actionbarEl = $compile(actionbarHtml)($scope);
+            $document.find('body').append(actionbarEl);
+            $scope.$digest();
+
 
         });
 
