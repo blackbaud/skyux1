b28178a65fb36da40ccd014dabe9b07d1ded6787
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-11-14 15:29:30 -0500

    set parallels-runs to specific number

--- scripts/browserstack-local-start.sh
+++ scripts/browserstack-local-start.sh
@@ -1,3 +1,3 @@
 # Assumes the binary is created and stored via the karma process.
-$BROWSER_STACK_BINARY_BASE_PATH/BrowserStackLocal -v --onlyAutomate --forcelocal --force --local-identifier SKYUXBROWSERSTACK --parallel-runs $BROWSER_STACK_ACCESS_KEY &
+$BROWSER_STACK_BINARY_BASE_PATH/BrowserStackLocal -v --onlyAutomate --forcelocal --force --local-identifier SKYUXBROWSERSTACK --parallel-runs=10 $BROWSER_STACK_ACCESS_KEY &
 sleep 3
