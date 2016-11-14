de599252d1124397694ae263cca4a968fa0d6dcd
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-11-14 16:00:33 -0500

    another browserstack tunnel attempt

--- scripts/browserstack-local-start.sh
+++ scripts/browserstack-local-start.sh
@@ -1,3 +1,3 @@
 # Assumes the binary is created and stored via the karma process.
-$BROWSER_STACK_BINARY_BASE_PATH/BrowserStackLocal -V --onlyAutomate --forcelocal --force --local-identifier SKYUXBROWSERSTACK --parallel-runs 10 --key $BROWSER_STACK_ACCESS_KEY &
+$BROWSER_STACK_BINARY_BASE_PATH/BrowserStackLocal --key $BROWSER_STACK_ACCESS_KEY -V --only-automate --force-local --force --local-identifier SKYUXBROWSERSTACK --parallel-runs 10 &
 sleep 3
