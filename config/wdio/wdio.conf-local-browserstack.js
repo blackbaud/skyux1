7e32283eddfd82bacf5d8893d652e0d292b0ae7d
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-11-16 10:22:21 -0500

    updated browserstack hub for SKY UX (#909)
    * updated browserstack hub for SKY UX
    
    * use specified browserstack flags
    
    * set parallels-runs to specific number
    
    * add more flags
    
    * another browserstack tunnel attempt
    
    * Add local identifier to wdio config
    
    * update browserstack binaries in an attempt to use local identifier
    
    * Update with appropriate browserstack options

--- config/wdio/wdio.conf-local-browserstack.js
+++ config/wdio/wdio.conf-local-browserstack.js
@@ -4,7 +4,8 @@
     'use strict';
 
     // Load our shared config
-    var shared = require('./wdio.conf-shared.js');
+    var shared = require('./wdio.conf-shared.js'),
+        timestamp = new Date().toString();
 
     shared.user = process.env.BROWSER_STACK_USERNAME;
     shared.key = process.env.BROWSER_STACK_ACCESS_KEY;
@@ -19,8 +20,10 @@
             browserDisconnectTolerance: 3,
             browserNoActivityTimeout: 3e5,
             captureTimeout: 3e5,
-            build: 'mac-chrome-webdriver-local',
-            resolution: '1280x960'
+            build: 'mac-chrome-webdriver-local-' + timestamp,
+            resolution: '1280x960',
+            'browserstack.localIdentifier': 'SKYUXBROWSERSTACKLOCAL',
+            name: 'SKYUXBROWSERSTACKLOCAL'
         }/*,
         {
             browserName: 'firefox',
@@ -61,7 +64,7 @@
             build: 'win8-ie10-webdriver-local'
         }*/
     ];
-    shared.host = 'hub-cloud.browserstack.com';
+    shared.host = 'hub.browserstack.com';
     shared.port = 80;
     shared.maxInstances = 1;
 
