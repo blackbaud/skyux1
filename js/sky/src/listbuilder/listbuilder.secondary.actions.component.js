cd62d031a2d0dbe2622c1e9b2172dc79d847acef
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-11-23 12:08:58 -0500

    start of secondary action work

---
+++ js/sky/src/listbuilder/listbuilder.secondary.actions.component.js
@@ -0,0 +1,29 @@
+/* global angular */
+(function () {
+    'use strict';
+
+    function Controller($scope) {
+        var ctrl = this;
+
+        function onInit() {
+            ctrl.secondaryMenuId = 'bb-listbuilder-secondary-actions-' + $scope.$id;
+        }
+
+        ctrl.$onInit = onInit;
+
+    }
+
+    Controller.$inject = ['$scope'];
+
+    angular.module('sky.listbuilder.secondary.actions.component', 
+        [
+            'ui.bootstrap.dropdown', 
+            'sky.resources', 
+            'sky.listbuilder.secondary.action.component'
+        ])
+        .component('bbListbuilderSecondaryActions', {
+            templateUrl: 'sky/templates/listbuilder/listbuilder.secondary.actions.component.html',
+            controller: Controller,
+            transclude: true
+        });
+})();
