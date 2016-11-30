047fe26687ef13e0d0acfea749d6ca54c49fb30b
rahul gupta <rahulitsmca@gmail.com>
2016-11-30 17:44:59 +0530

    Removed ng-class from the html and added in the content-custom-item directive

--- js/sky/src/splitpanel/splitpanel.content.custom.item.directive.js
+++ js/sky/src/splitpanel/splitpanel.content.custom.item.directive.js
@@ -2,23 +2,19 @@
 (function () {
     'use strict';
 
-    function linkFn(scope, el, attr, customCtrl) {
-        customCtrl.addItem();
-    }
-
     angular.module('sky.splitpanel.content.custom.item.directive', [])
         .directive('bbSplitpanelContentCustomItem', function () {
             return {
                 templateUrl: 'sky/templates/splitpanel/splitpanel.content.item.component.html',
                 transclude: true,
+                replace: true,
                 restrict: 'A',
-                link: linkFn,
                 require: '^^bbSplitpanelContentCustom',
                 controller: function ($scope) {
                     var ctrl = this;
-                    $scope.selectItem = function (currentScope) {
-                        currentScope.item.$index = currentScope.$index;
-                        this.$parent.$parent.$parent.$ctrl.listbuilderContentCtrl.OnSelectedItem(currentScope.item);
+                    $scope.selectItem = function () {
+                        $scope.item.$index = $scope.$index;
+                        $scope.$parent.$parent.$parent.$ctrl.listbuilderContentCtrl.OnSelectedItem($scope.item);
                     }
                 }
             };
