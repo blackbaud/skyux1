10e6bd370bfd27a4506b2c2b3ed147c9069974d8
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-12-07 09:47:01 -0500

    Add test stubs for listbuilder grid view

---
+++ js/sky/src/listbuilder/test/listbuilder.grid.component.spec.js
@@ -0,0 +1,55 @@
+/* jshint jasmine: true */
+/* global module, angular, inject */
+(function () {
+    'use strict';
+
+    describe('listbuilder grid view', function () {
+
+        beforeEach(module(
+            'sky.listbuilder',
+            'sky.grids',
+            'sky.templates'
+        ));
+
+        it('uses the listbuilder toolbar as the boundary element for the grid header view keepers', function () {
+
+        });
+
+        it('allows header based sort to occur in grids if the sort component does not exist', function () {
+
+        });
+
+        it('does not allow header based sort to occur in grids if the sort component does exist', function () {
+
+        });
+
+        it('scrolls the listbuilder top scrollbar element when grid table wrapper is scrolled', function () {
+
+        });
+
+        it('scrolls the grid table wrapper element when top scrollbar is scrolled', function () {
+
+        });
+
+        it('listens for selecting and unselecting items in grid', function () {
+
+        });
+
+        it('applies select all and clear all properly when selectedids are hooked up', function () {
+
+        });
+
+        it('hides the select all and clear all checkbox in the grid headers', function () {
+
+        });
+
+        it('allows use of the column picker component', function () {
+
+        });
+
+        it('destroys column picker component when not in grid view', function () {
+
+        });
+
+    });
+})();
