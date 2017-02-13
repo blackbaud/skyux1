4917f27283417c831bad3c1662220978040a4dd4
Blackbaud-DanielCooke <daniel.cooke@blackbaud.com>
2017-02-13 16:03:41 -0500

    Tabbed sectioned form implementation (#988)
    * Initial tabbed section modal
    
    * Changed sectioned form validation to use nested ng-forms instead of a validation service.
    
    * Adding sectioned form settings in demo markdown
    
    * Converting sectioned forms to use events for handling the visibility of sections.
    
    * Changing the style of border between sections and content on sectioned forms.
    
    * Getting rid of back to section navigation now that events are working.
    
    * Adding tests for sectioned form
    
    * Adding tests for sectioned modal
    
    * Converting sectioned form directive to component.
    
    * Renaming sectioned form and modal files with part suffix.
    
    * Moving sectioned form module declaration to its own file.
    
    * Moving sectioned form media breakpoints registration to onInit/Destroy
    
    * Removing scope reference for media breakpoints on sectioned form component.
    
    * Fixing js lint error in sectioned form.
    
    * Changing sectioned form to take a callback for section visibility instead of emitting an event.
    
    * Updating sectioned form to use the tab select callback instead of $scope.$watch on the active tab
    
    There was also a side effect of watching the active tab in that when the
    form was loaded in mobile the active tab was changed, thus hiding the
    tab content by default.  The mediaBreakpointHandler does not work in
    this case because it is first run (on registration) in $onInit which has
    not loaded child elements.  To overcome this added a $postLink function
    that sets the initial form state after child elements are loaded.
    
    * Make parent form optional on sectioned form
    
    * Ignore code coverage for else, do nothing branches in sectioned form
    
    * Adding note about optional parent form and modal styling to sectioned form documentation.
    
    * Updating sectioned form styling to toggle based on screen width so we don't need !important
    
    * Making parent form required for sectioned form per code review.
    
    Also added some comments to expand on why code coverage was disabled.
    
    * Adding comment to sectioned form styling on why !important is required for the active tab section.
    
    * Adding visual tests for sectioned form
    
    * Removing some leftover unused code in the sectioned form demo

---
+++ js/sky/src/sectionedform/test/sectionedform.component.spec.js
@@ -0,0 +1,179 @@
+/*jshint jasmine: true */
+/*global angular */
+
+describe('SectionedForm', function () {
+    'use strict';
+
+    var $compile,
+        $document,
+        $scope,
+        $templateCache,
+        bbMediaBreakpoints;
+
+    beforeEach(function () {
+        angular.mock.module(
+            'sky.templates',
+            'sky.sectionedform',
+            'uib/template/tabs/tabset.html',
+            'uib/template/tabs/tab.html'
+        );
+
+        angular.mock.inject(function (_$rootScope_, _$compile_, _$document_, _$templateCache_, _bbMediaBreakpoints_) {
+            $scope = _$rootScope_.$new();
+            $compile = _$compile_;
+            $document = _$document_;
+            $templateCache = _$templateCache_;
+            bbMediaBreakpoints = _bbMediaBreakpoints_;
+        });
+    });
+
+    function compileSectionedForm() {
+        var el = $compile(
+            '<form>' +
+                '<bb-sectioned-form bb-sectioned-form-sections="sections"></bb-sectioned-form>' +
+            '</form>')($scope);
+
+        $scope.$digest();
+
+        return el.children();
+    }
+
+    function getContentElement(el) {
+        return el.find('div.tab-content:first');
+    }
+
+    function getFirstSectionTabElement(el) {
+        return el.find('uib-tab-heading:first > span:first');
+    }
+
+    function getSectionsElement(el) {
+        return el.find('ul.nav-tabs:first');
+    }
+
+    function expectSectionsHiddenContentVisible(el) {
+        expect(getSectionsElement(el)).toBeHidden();
+        expect(getContentElement(el)).toBeVisible();
+    }
+
+    function expectSectionsVisibleContentHidden(el) {
+        expect(getSectionsElement(el)).toBeVisible();
+        expect(getContentElement(el)).toBeHidden();
+    }
+
+    describe('component', function () {
+        it('should change the style of a section if a required field exists in the content', function () {
+            var sutView;
+            
+            $scope.sections = [
+                {
+                    formName: 'formName',
+                    templateUrl: 'test/sectionedform/form.html'
+                }
+            ];
+
+            $templateCache.put(
+                'test/sectionedform/form.html',
+                '<ng-form name="formName">' +
+                    '<input name="reqField" ng-model="reqField" required />' +
+                '</ng-form>');
+
+            sutView = compileSectionedForm();
+
+            expect(getFirstSectionTabElement(sutView)).toHaveClass('required');
+        });
+
+        it('should change the style of a section if the section is invalid', function () {
+            var sutController,
+                sutView;
+            
+            $scope.sections = [
+                {
+                    formName: 'formName',
+                    templateUrl: 'test/sectionedform/form.html'
+                }
+            ];
+
+            $templateCache.put(
+                'test/sectionedform/form.html',
+                '<ng-form name="formName">' +
+                    '<input name="reqField" ng-model="reqField" required />' +
+                '</ng-form>');
+
+            sutView = compileSectionedForm();
+
+            sutController = sutView.controller('bbSectionedForm');
+            sutController.form.$submitted = true;
+            $scope.$digest();
+
+            expect(getFirstSectionTabElement(sutView)).toHaveClass('invalid');
+            expect(getFirstSectionTabElement(sutView)).not.toHaveClass('required');
+        });
+
+        it('should show the item count in the section heading', function () {
+            var sutView;
+
+            $scope.sections = [
+                {
+                    heading: 'testHeading',
+                    itemCount: 5
+                }
+            ];
+
+            sutView = compileSectionedForm();
+
+            expect(getFirstSectionTabElement(sutView)).toHaveText('testHeading (5)');
+        });
+        
+        it('should transition from sections to content when a section is selected on mobile', function () {
+            var bbMediaBreakpointsCallback,
+                sutController,
+                sutView;
+
+            $scope.sections = [{}];
+            spyOn(bbMediaBreakpoints, 'register').and.callFake(function (callback) {
+                bbMediaBreakpointsCallback = callback;
+            });
+
+            sutView = compileSectionedForm();
+            $document.find('body:first').append(sutView);
+
+            sutController = sutView.controller('bbSectionedForm');
+            bbMediaBreakpointsCallback({xs: true});
+            $scope.$digest();
+
+            expectSectionsVisibleContentHidden(sutView);
+
+            sutController.activeSection = 0;
+            $scope.$digest();
+
+            expectSectionsHiddenContentVisible(sutView); 
+        });
+        
+        it('should transition from content to sections when event is triggered on mobile', function () {
+            var bbMediaBreakpointsCallback,
+                sutController,
+                sutView;
+
+            $scope.sections = [{}];
+            spyOn(bbMediaBreakpoints, 'register').and.callFake(function (callback) {
+                bbMediaBreakpointsCallback = callback;
+            });
+
+            sutView = compileSectionedForm();
+            $document.find('body:first').append(sutView);
+
+            sutController = sutView.controller('bbSectionedForm');
+            bbMediaBreakpointsCallback({xs: true});
+            $scope.$digest();
+
+            sutController.activeSection = 0;
+            $scope.$digest();
+
+            expectSectionsHiddenContentVisible(sutView);
+
+            $scope.$broadcast('reinitializeSectionDisplay');
+
+            expectSectionsVisibleContentHidden(sutView);
+        });
+    });
+});
