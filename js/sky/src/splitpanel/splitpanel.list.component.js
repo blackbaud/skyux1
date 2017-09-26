1613440a7bfd1736eb1aa0d4190cead9623b3562
amitansu9988 <amitansu9988@gmail.com>
2017-09-26 17:19:27 +0530

    Correct indentation

--- js/sky/src/splitpanel/splitpanel.list.component.js
+++ js/sky/src/splitpanel/splitpanel.list.component.js
@@ -5,6 +5,6 @@
     angular.module('sky.splitpanel.list.directive', [])
         .component('bbSplitpanelList', {
             templateUrl: 'sky/templates/splitpanel/splitpanel.list.component.html',
-                transclude: true
+            transclude: true
         });
 })();
