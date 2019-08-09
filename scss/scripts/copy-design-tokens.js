b7272ae2609fa6a9eb4c1d725f6945af9febf74b
Bobby Earl <bobby.earl@blackbaud.com>
2019-08-09 10:34:59 -0400

    Updated to use fs-extra methods.

--- scss/scripts/copy-design-tokens.js
+++ scss/scripts/copy-design-tokens.js
@@ -1,13 +1,11 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
-const dir = path.resolve(process.cwd(), 'scss/vendor');
+const vendor = path.resolve(process.cwd(), 'scss/vendor');
 
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir);
-}
+fs.ensureDirSync(vendor);
 
 fs.copyFileSync(
   path.resolve(process.cwd(), 'node_modules/@blackbaud/skyux-design-tokens/scss/variables.scss'),
-  path.resolve(dir, 'skyux-design-tokens-variables.scss')
+  path.resolve(vendor, 'skyux-design-tokens-variables.scss')
 );
